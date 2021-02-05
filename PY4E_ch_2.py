29c4fb9bb272d5c92e7be6f18fffa6480c69096b
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-04 21:06:25 -0700

    Update PY4E_ch_2.py

--- PY4E_ch_2.py
+++ PY4E_ch_2.py
@@ -1,4 +1,8 @@
 #Write a program to prompt the user for hours and rate per hour using input
+======
+#2.3 Write a program to prompt the user
+#for hours and rate per hour using input
+======
 
 try :
     hrs = int(input('Hours: '))
