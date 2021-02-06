1e5dd0de0720ccebf883b0690e2ce18e7d755c7f
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-06 13:10:41 -0700

    Update FIZZBUZZ.py
    added symbols for readibility, to track differences

--- FIZZBUZZ.py
+++ FIZZBUZZ.py
@@ -2,24 +2,24 @@ print("START")
 
 for user_input in range(1, 101):
     if user_input % 3 == 0 and user_input % 5 == 0:
-        str = "{}) fizzbuzz"
+        str = "{} + fizzbuzz"
         print (str.format(user_input))
 
     elif user_input % 3 == 0:
-        str = "{}) fizz"
+        str = "{} > fizz"
         print (str.format(user_input))
 
     if user_input % 5 == 0:
-        str = "{}) buzz"
+        str = "{} ? buzz"
         print (str.format(user_input))
 
-print("FIN")        
+print("FIN")
 
 
 
 # user_input = 15
 
-# if user_input % 3 == 0: 
+# if user_input % 3 == 0:
 #     print(“FIZZ”)
 
 # if user_input % 5 == 0:
