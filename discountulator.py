5f2d92ad1f6ba4747956cd0494de352f2da7f5f7
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-06 12:41:12 -0700

    Update discountulator.py
    added procedures for discount min, and "else" clause for when discount min is not met

--- discountulator.py
+++ discountulator.py
@@ -11,6 +11,12 @@ if inp_discount_min == 0 :
     user_discount_total = ((inp_discount_rate / 100) * inp_cart)
     print('you saved: ', user_discount_total)
     print('new total: ', inp_cart - user_discount_total)
+elif inp_discount_min >= 0 and inp_discount_min <= inp_cart:
+    user_discount_total = ((inp_discount_rate / 100) * inp_cart)
+    print('you saved: ', user_discount_total)
+    print('new total: ', inp_cart - user_discount_total)
+else :
+    print('you have not met the minimum cart total to qualify for a discount')
 
 #else :
     #ot = (hrs - 40) * (rate * 1.5)
