53403783216b4ea92eabfa6d212ff46062614a08
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-08 13:36:06 -0700

    Update PY4E_ch_3_A.py
    working on defining a function for the regular rate calculation  procedure

--- PY4E_ch_3_A.py
+++ PY4E_ch_3_A.py
@@ -1,15 +1,22 @@
 #Write a program to prompt the user for hours and rate per hour using input
 #with 1.5% overtime
 
+
 try :
     hrs = float(input('hours: '))
     rate = float(input('hourly rate: '))
 except :
     print('numbers only')
 
-if hrs <= 40 :
-    print('regular pay: ', hrs * rate)
+def reg_pay_calc () :
+    total_reg_pay = hrs * rate
+    print(total_reg_pay)
 
+if hrs <= 40 :
+    print('regular pay:')
+    reg_pay_calc()
 else :
     ot = (hrs - 40) * (rate * 1.5)
+    #print('overtime pay:')
+    #reg_pay_calc(+ ot)
     print('overtime pay: ', (40 * rate) + ot)
