c8f782e966ced00cb0cd801afe8e62d929ebb776
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-08 21:20:42 -0700

    Update PY4E_ch_3_A.py
    added pay_calc function to house calculations

--- PY4E_ch_3_A.py
+++ PY4E_ch_3_A.py
@@ -2,21 +2,40 @@
 #with 1.5% overtime
 
 
+#try :
+    #hrs = float(input('hours: '))
+    #rate = float(input('hourly rate: '))
+#except :
+    #print('numbers only')
+
+#def reg_pay_calc () :
+    #total_reg_pay = hrs * rate
+    #print(total_reg_pay)
+
+#if hrs <= 40 :
+    #print('regular pay:')
+    #reg_pay_calc()
+#else :
+    #ot = (hrs - 40) * (rate * 1.5)
+    #print('overtime pay:')
+    #reg_pay_calc(+ ot)
+    #print('overtime pay: ', (40 * rate) + ot)
+def pay_calc(hours, rate) :
+    if hours >40 :
+        reg = rate * hours
+        otp = (hours - 40) * (rate * 0.5)
+        x = reg + otp
+        print('Pay', x)
+    else :
+        x = reg
+        print('Pay', x)
+    return x
+
 try :
     hrs = float(input('hours: '))
-    rate = float(input('hourly rate: '))
+    r = float(input('hourly rate: '))
 except :
     print('numbers only')
 
-def reg_pay_calc () :
-    total_reg_pay = hrs * rate
-    print(total_reg_pay)
-
-if hrs <= 40 :
-    print('regular pay:')
-    reg_pay_calc()
-else :
-    ot = (hrs - 40) * (rate * 1.5)
-    #print('overtime pay:')
-    #reg_pay_calc(+ ot)
-    print('overtime pay: ', (40 * rate) + ot)
+    
+pay = pay_calc(hrs,r)
