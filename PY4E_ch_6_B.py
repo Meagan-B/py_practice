5cef79681891a371d14166905e0a5e9ac7415bde
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-10 13:34:50 -0700

    Create PY4E_ch_6_B.py
    PY4E indexing tests

---
+++ PY4E_ch_6_B.py
@@ -0,0 +1,12 @@
+# PY4E understanding indexing
+
+fruit = 'pineapple'
+
+print(fruit[:3])
+print('pin')
+print(fruit[3:6])
+print('eap')
+print(fruit[5:])
+print('apple')
+print(fruit[:])
+print('_')
