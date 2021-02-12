5cd94a02e5c059d78febf4656139e2ed3c7eceb6
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-11 22:38:45 -0700

    Update invoice_data.py
    finished data entry for 2020 invoices

--- invoice_data.py
+++ invoice_data.py
@@ -7,81 +7,16 @@ inv_05 = ['012', 'DLC Innovation', '(954) 404-2692', 'Amanda & Matt Full wedding
 inv_06 = ['003', 'DLC Innovation', '(954) 404-2692', 'Imani and Ish Highlight and teaser (free)', '12-31-2019', '300.00', '300.00', '000.00']
 inv_07 = ['005', 'DLC Innovation', '(954) 404-2692', 'Imani and Ish 2 bonus features', '01-23-2020', '200.00', '200.00', '000.00']
 inv_08 = ['018', 'Halfmoon Film Company', '(814) 380-4148', 'Ashley + Luke; teaser, highlight, ceremony, reception', '09-08-2020', '750.00', '750.00', '000.00']
-inv_08 = ['023', 'Halfmoon Film Company', '(814) 380-4148', 'Jen + Brian; teaser, highlight, ceremony, reception', '11-04-2020', '750.00', '750.00', '000.00']
 inv_09 = ['020', 'Halfmoon Film Company', '(814) 380-4148', 'Kasey + Kyle; teaser, highlight, ceremony, reception', '09-15-2020', '750.00', '750.00', '000.00']
-inv_10 = ['021', 'Blue Truck Media', '(972) 672-9122', 'NSU 2020 edits', '10-26-2020', '1,650.00', '1,650.00', '000.00']
+inv_10 = ['021', 'Blue Truck Media', '(972) 672-9122', 'NSU 2020 edits', '10-26-2020', '1650.00', '1650.00', '000.00']
 inv_11 = ['011', 'DLC Innovation', '(954) 404-2692', 'Amanda & Matt trailer','04-27-2020','200.00', '200.00', '000.00']
-inv_12 = [014
-Reynaldo Martin Studios
-(954) 817-9568
-(2) Debra and paula, shorts and teasers
-Aug 10, 2020
-$1,100.00
-$1,100.00
-$000.00
-]
-inv_13 = [013
-Reynaldo Martin Studios
-(954) 817-9568
-Wedding and trailer, Katie and Zach
-Sept. 1, 2020
-$760.00
-$760.00
-$000.00]
-inv_14 = [008
-Reynaldo Martin Studios
-(954) 817-9568
-(3) Weddings and trailers, Kristina, Lisa & Monique
-Feb 11, 2020
-$2,280.00
-$1,140.00
-$1,140.00 ]
-inv_15 = [008
-Reynaldo Martin Studios
-(954) 817-9568
-(3) Weddings and trailers, Kristina, Lisa & Monique
-April 17, 2020
-$2,280.00
-$1,140.00
-$000.00]
-inv_16 = [019
-Reynaldo Martin Studios
-(954) 817-9568
-Nicola, wedding and teaser, no color/sound
-09/08/2020
-$600.00
-$600.00
-$000.00]
-inv_17 = [004
-Reynaldo Martin Studios
-(954) 817-9568
-(3) Weddings and trailers, Raisa, Sarah & Afiya
-Dec 31, 2019
-$2,280.00
-$1,140.00
-$1,140.00 ]
-inv_18 = [009
-Reynaldo Martin Studios
-(954) 817-9568
-(3) Weddings and trailers, Raisa, Sarah & Afiya
-April 1, 2020
-$2,280.00
-$1,140.00
-$000.00 ]
-inv_19 = [016
-Janett Ross
-(520) 368-3232
-Why Arizona 90 sec promo edit, 3 selects, graphic titles + additional clip ($40)
-July 15, 2020
-$790.00
-$790.00
-$000.00]
-inv_20 = [017
-DLC Innovation
-(954) 404-2692
-Rayssi (1), Lucika & Salim (2), Lux BTS (1), Sherene & Seymour (2), Ahtziri & Le’Vitria (2)
-09/18/2020
-$2,400.00
-$550.00
-$1,850.00  ]
-inv_21 =
+inv_12 = ['014', 'Reynaldo Martin Studios', '(954) 817-9568', '(2) Debra and paula, shorts and teasers', '08-10-2020', '1100.00', '1100.00', '000.00']
+inv_13 = ['013', 'Reynaldo Martin Studios', '(954) 817-9568', 'Wedding and trailer, Katie and Zach', '09-01-2020', '760.00', '760.00', '000.00']
+inv_14 = ['008', 'Reynaldo Martin Studios', '(954) 817-9568', '(3) Weddings and trailers, Kristina, Lisa & Monique', '02-11-2020', '2280.00', '1140.00', '1140.00']
+inv_15 = ['008', 'Reynaldo Martin Studios', '(954) 817-9568', '(3) Weddings and trailers, Kristina, Lisa & Monique', '04-17-2020', '2280.00', '1140.00', '000.00']
+inv_16 = ['019', 'Reynaldo Martin Studios', '(954) 817-9568', 'Nicola, wedding and teaser, no color/sound', '09-08-2020', '600.00', '600.00', '000.00']
+inv_17 = ['004', 'Reynaldo Martin Studios', '(954) 817-9568', '(3) Weddings and trailers, Raisa, Sarah & Afiya', '12-31-2019', '2280.00', '1140.00', '1140.00']
+inv_18 = ['009', 'Reynaldo Martin Studios', '(954) 817-9568', '(3) Weddings and trailers, Raisa, Sarah & Afiya', '04-01-2020', '2280.00', '1140.00', '000.00']
+inv_19 = ['016', 'Janett Ross', '(520) 368-3232', 'Why Arizona 90 sec promo edit, 3 selects, graphic titles + additional clip ($40)', '07-15-2020', '790.00', '790.00', '000.00']
+inv_20 = ['017', 'DLC Innovation', '(954) 404-2692', 'Rayssi (1), Lucika & Salim (2), Lux BTS (1), Sherene & Seymour (2), Ahtziri & Le’Vitria (2)', '09-18-2020', '2400.00', '550.00', '1850.00']
+inv_21 = ['023', 'Halfmoon Film Company', '(814) 380-4148', 'Jen + Brian; teaser, highlight, ceremony, reception', '11-04-2020', '750.00', '750.00', '000.00']
