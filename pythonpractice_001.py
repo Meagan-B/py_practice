1fe6db90546547bfda1ae6bb0f3afa2fbe6c02c1
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-12 23:17:52 -0700

    Update pythonpractice_001.py
    cosmetic changes to >100

--- pythonpractice_001.py
+++ pythonpractice_001.py
@@ -30,7 +30,7 @@ while True:
         print(dysto100, dyscalc)
         break
     else :
-        print('you win life! you have already made it to 100' * 100 )
+        print('you WIN @ life!  ' * 100 )
         break
-        
+
 print('thank you for playing!')
