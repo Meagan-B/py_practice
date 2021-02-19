cd1a5fbf9060ec6a8430a9116580044625826112
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-18 17:19:34 -0700

    Update PY4E_ch_7_B.py

--- PY4E_07/PY4E_ch_7_B.py
+++ PY4E_07/PY4E_ch_7_B.py
@@ -1,16 +1,14 @@
 # Exercise 2: Write a program to prompt for a file name, and then read through the file and look for lines of the form:
-#
 # X-DSPAM-Confidence: 0.8475
 # When you encounter a line that starts with “X-DSPAM-Confidence:” pull apart the line to extract the floating-point number on the line.
 # Count these lines and then compute the total of the spam confidence values from these lines. When you reach the end of the file,
 # print out the average spam confidence.
-#
 # Enter the file name: mbox.txt
 # Average spam confidence: 0.894128046745
-#
 # Enter the file name: mbox-short.txt
 # Average spam confidence: 0.750718518519
 # Test your file on the mbox.txt and mbox-short.txt files.
+
 
 #WIP
 
@@ -28,6 +26,7 @@ read_fhandle = fhandle.readlines()
 
 
 print(fhandle)
+<<<<<<< Updated upstream
 
 spam_count = 0
 spam_total = 0.000000000000
@@ -52,6 +51,29 @@ for line in read_fhandle :
 # print('Average spam confidence: %s' % spam_avg)
 
 
+=======
+
+spam_count = 0
+spam_total = 0.000000000000
+#      line_clean = line.rstrip()
+for line in read_fhandle :
+    if 'X-DSPAM-Confidence: ' in line :
+        spam_val = read_fhandle.split(':')
+        print(spam_val)
+        spam_count += 1
+        print(spam_count, ')', spam_val)
+    try :
+        float_val = float(spam_val)
+    except :
+        print('pos, could not convert float from line')
+
+spam_total = spam_total + float_val
+print('TOTAL: ', spam_total)
+spam_avg = spam_total / spam_count
+print('Average spam confidence: %s' % spam_avg)
+
+#
+>>>>>>> Stashed changes
 # spam_count = 0
 # spam_total = 0
 # for line in fhandle :
@@ -60,7 +82,13 @@ for line in read_fhandle :
 #     spam_count += 1
 #     spam_val = line_clean.split(':')
 #     print(spam_val, spam_count)
+<<<<<<< Updated upstream
     # spam_total = spam_total + spam_val
     # float_val = float(spam_val)
     # spam_avg = spam_total / spam_count
+=======
+#     spam_total = spam_total + spam_val
+#     float_val = float(spam_val)
+#     spam_avg = spam_total / spam_count
+>>>>>>> Stashed changes
 # print('Average spam confidence: %s' % spam_avg)
