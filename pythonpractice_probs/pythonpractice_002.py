c89719582386c26e313d9445fce4254b70533653
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-22 21:34:00 -0700

    Update pythonpractice_002.py
    comment out on prompt

--- pythonpractice_probs/pythonpractice_002.py
+++ pythonpractice_probs/pythonpractice_002.py
@@ -1,9 +1,11 @@
-Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
-
-Extras:
-
-If the number is a multiple of 4, print out a different message.
-Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+# Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. 
+# Hint: how does an even / odd number react differently when divided by 2?
+#
+# Extras:
+#
+# If the number is a multiple of 4, print out a different message.
+# Ask the user for two numbers: one number to check (call it num) and one number to divide by (check).
+# If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
 
 num = input("enter a number >>")
 try :
@@ -12,12 +14,10 @@ except :
     if num != int() :
         print("numbers only >>> ")
         continue
-   
+
 if fl_num % 2 == 0 :
     print("%s is even" % fl_num)
 elif fl_num % 4 == 0 :
     print("%s is MAGIC" % fl_num)
 else :
     print("%s is odd" % fl_num)
-        
-    
