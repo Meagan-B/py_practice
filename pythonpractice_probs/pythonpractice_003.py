fd6ca75146d84a84b18522ef22c599e646d75550
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-23 20:40:35 -0700

    Create pythonpractice_003.py

---
+++ pythonpractice_probs/pythonpractice_003.py
@@ -0,0 +1,20 @@
+# write a program that prints out all the elements of the list that are less than 5.
+#
+# Extras:
+#
+# Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and
+# print out this new list.
+#
+# Write this in one line of Python.
+#
+# Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+
+num_list = [1, 3, 2, 4, 19, 23, 278, 10, 736, 0.5]
+
+for num in num_list :
+    if num < 5 :
+        print(num)
+    else :
+        continue
+            
