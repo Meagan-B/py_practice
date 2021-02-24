464ebbde645bc52a9f65fbb3f85282e38de9e7b7
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-24 15:47:27 -0700

    Update PY4E_ch_8_A.py
    both prompts, parsing function creation, fully functional.

--- PY4E_08/PY4E_ch_8_A.py
+++ PY4E_08/PY4E_ch_8_A.py
@@ -1,22 +1,19 @@
 # Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None.
 
-
-
 def chop(l) :
     del(l[0])
     del(l[-1])
 
 test_list = ['cats', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'f']
 
-
 chop(test_list)
 print(test_list)
 
 
-
-
-
-
-
-
 # Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
+
+def middle(l) :
+    return test_list[1:]
+
+middle(test_list)
+print(test_list)    
