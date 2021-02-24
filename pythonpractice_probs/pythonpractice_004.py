cfb472ec05905275286e713d5e011d3813238c9f
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-23 21:23:55 -0700

    Create pythonpractice_004.py

---
+++ pythonpractice_probs/pythonpractice_004.py
@@ -0,0 +1,2 @@
+# Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+# For example, 13 is a divisor of 26 because 26 / 13 has no remainder.
