2f094714d92fe839f68e2facd79e3035f62ab23b
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-24 18:33:19 -0700

    Create PY4E_ch_8_C.py

---
+++ PY4E_08/PY4E_ch_8_C.py
@@ -0,0 +1,22 @@
+# Exercise 2: Figure out which line of the above program is still not properly guarded. See if you can construct a text file which
+# causes the program to fail and then modify the program so that the line is properly guarded and test it to make sure it handles your
+# new text file.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Exercise 3: Rewrite the guardian code in the above example without two if statements. Instead, use a compound logical expression
+# using the or logical operator with a single if statement.
