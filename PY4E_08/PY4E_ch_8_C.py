8906715f9ee66d2bb7c0bca525cac276885aebfc
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-26 15:39:40 -0700

    Update PY4E_ch_8_C.py
    added PY4E code to debug

--- PY4E_08/PY4E_ch_8_C.py
+++ PY4E_08/PY4E_ch_8_C.py
@@ -1,23 +1,16 @@
-# Exercise 2: Figure out which line of the above program is still not properly guarded. See if you can construct a text file which
+# Exercise 2: Figure out which line of the below program is still not properly guarded. See if you can construct a text file which
 # causes the program to fail and then modify the program so that the line is properly guarded and test it to make sure it handles your
 # new text file.
 
 
-
-
-
-
-
-
-
-
-fruit = 'Banana'
-fruit[0] = 'b'
-print(fruit)
-
-
-
-
+fhand = open('mbox-short.txt')
+count = 0
+for line in fhand:
+    words = line.split()
+    # print('Debug:', words)
+    if len(words) == 0 : continue
+    if words[0] != 'From' : continue
+    print(words[2])
 
 
 # Exercise 3: Rewrite the guardian code in the above example without two if statements. Instead, use a compound logical expression
