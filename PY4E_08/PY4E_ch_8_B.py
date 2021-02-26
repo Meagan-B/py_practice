019cbd1899a80cbda7241caa6982d53c09c2c149
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-25 20:06:23 -0700

    Update PY4E_ch_8_B.py

--- PY4E_08/PY4E_ch_8_B.py
+++ PY4E_08/PY4E_ch_8_B.py
@@ -1,10 +1,10 @@
 # PY4E experimenting with lists
-
-t.append(x)
-t = t + [x]
-And these are wrong:
-
-t.append([x])          # WRONG!
-t = t.append(x)        # WRONG!
-t + [x]                # WRONG!
-t = t + x
+#
+# t.append(x)
+# t = t + [x]
+# And these are wrong:
+#
+# t.append([x])          # WRONG!
+# t = t.append(x)        # WRONG!
+# t + [x]                # WRONG!
+# t = t + x
