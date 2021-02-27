07759932bd95e8d73b92bf1d831fd408cee05f5b
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-02-26 18:11:48 -0700

    Update PY4E_ch_8_D.py
    working, needs alphabetizing

--- PY4E_08/PY4E_ch_8_D.py
+++ PY4E_08/PY4E_ch_8_D.py
@@ -9,3 +9,14 @@
 # split the line into a list of words using the split function. For each word,
 # check to see if the word is already in the list of unique words. If the word is not in the list of unique words,
 # add it to the list. When the program completes, sort and print the list of unique words in alphabetical order.
+
+ss_uniq_wrds = []
+
+usr_inp = input('enter file name >>> ')
+fhandle = open(usr_inp)
+
+for lines in fhandle :
+    words = lines.split()
+    ss_uniq_wrds += words
+
+print(ss_uniq_wrds)
