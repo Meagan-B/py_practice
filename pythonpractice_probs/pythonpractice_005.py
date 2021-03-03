28c7a5f69da0af4ac6fc3a490e8f2472765f835b
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-03 12:24:25 -0700

    Update pythonpractice_005.py

--- pythonpractice_probs/pythonpractice_005.py
+++ pythonpractice_probs/pythonpractice_005.py
@@ -11,6 +11,7 @@
 # Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
 #
 # >>>>>>>>>>>>>>>>>>>>>
+# WIP
 
 a_lst = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
