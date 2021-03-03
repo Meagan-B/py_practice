ddc742ea5c17ab7a710a3719e52875fad8da5658
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-02 21:53:12 -0700

    Update pythonpractice_005.py

--- pythonpractice_probs/pythonpractice_005.py
+++ pythonpractice_probs/pythonpractice_005.py
@@ -15,3 +15,14 @@
 
 a_lst = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c_lst = [a_lst, b_lst]
+e_lst = []
+
+for items in c_kst :
+    if item in a_list
+
+
+print(c_lst)
+
+for
+# e_lst = list()
