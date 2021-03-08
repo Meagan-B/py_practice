8dff3034b56f4c963cf2cc8790c4afaa47360f3f
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-08 14:50:59 -0700

    Update PY4E_ch_10_B.py
    changes to notes

--- PY4E_10/PY4E_ch_10_B.py
+++ PY4E_10/PY4E_ch_10_B.py
@@ -7,3 +7,45 @@
 # sorted by hour as shown below.
 # #
 # -------------------------------
+
+usr_inp = input('enter file name >>> ')
+f_handl = open(usr_inp)
+
+
+d = dict()
+for line in f_handl:
+    if line.startswith('From ') :
+        print(line)
+        l_splt = line.split()
+        # print(l_splt[1])
+        # e_addr = l_splt[1]
+        hrs = l_splt[5]
+        time_brk = hrs.split(':')
+        hrs_ext = time_brk[0]
+        print(hrs_ext)
+        # d[domain] = d.get(domain, 0) + 1
+        # d[uname] = d.get(uname, 0) + 1
+        # d[hrs] = d.get(hrs, 0) + 1
+
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+# print(sorted([(v, k) for k, v in d.items()], reverse=True))
+
+
+# def dct_max_fun(d) :
+#     max_val = 0
+#     max_key = None
+#     for key,count in d.items():
+# # code from PY4E discussion, by Arran Patterson
+# # if max is None or count > bigcount:
+#         if count > max_val :
+#             max_val = count
+#             max_key = key
+#     # print(max_key, max_val)
+#     print('the MOST emails, {0}, were sent by {1}'.format(max_val, max_key))
+
+# dct_max_fun(d)
+
+#  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+### copypaste of code from PY4E_ch_9_E
