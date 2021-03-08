f5369100a4edd5c0ac2bae4a162412fb86ccd584
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-08 14:36:07 -0700

    Update PY4E_ch_10_A.py
    working off of code from PY4E_ch_9_E

--- PY4E_10/PY4E_ch_10_A.py
+++ PY4E_10/PY4E_ch_10_A.py
@@ -8,3 +8,41 @@
 # Then sort the list in reverse order and print out the person who has the most commits.
 #
 #-----------------------------------
+
+usr_inp = input('enter file name >>> ')
+f_handl = open(usr_inp)
+
+
+d = dict()
+for line in f_handl:
+    if line.startswith('From:') :
+        # print(line)
+        l_splt = line.split()
+        # print(l_splt[1])
+        e_addr = l_splt[1]
+        uname, domain = e_addr.split('@')
+        # print(domain)
+        d[domain] = d.get(domain, 0) + 1
+
+
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+print(sorted([(v, k) for k, v in d.items()], reverse=True))
+
+def dct_max_fun(d) :
+    max_val = 0
+    max_key = None
+    for key,count in d.items():
+# code from PY4E discussion, by Arran Patterson
+# if max is None or count > bigcount:
+        if count > max_val :
+            max_val = count
+            max_key = key
+    # print(max_key, max_val)
+    print('the MOST emails, {0}, were sent by {1}'.format(max_val, max_key))
+
+dct_max_fun()    
+
+#  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+### copypaste of code from PY4E_ch_8_D
