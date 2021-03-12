bd2bdf577cf169301bc054212fad5d412986a50c
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-11 23:05:34 -0700

    Update PY4E_ch_3_A.py
    new notes

--- PY4E_03/PY4E_ch_3_A.py
+++ PY4E_03/PY4E_ch_3_A.py
@@ -1,41 +1,35 @@
 #Write a program to prompt the user for hours and rate per hour using input
 #with 1.5% overtime
+#
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-
-#try :
-    #hrs = float(input('hours: '))
-    #rate = float(input('hourly rate: '))
-#except :
-    #print('numbers only')
-
-#def reg_pay_calc () :
-    #total_reg_pay = hrs * rate
-    #print(total_reg_pay)
-
-#if hrs <= 40 :
-    #print('regular pay:')
-    #reg_pay_calc()
-#else :
-    #ot = (hrs - 40) * (rate * 1.5)
-    #print('overtime pay:')
-    #reg_pay_calc(+ ot)
-    #print('overtime pay: ', (40 * rate) + ot)
+# defining a function to calculate pay
 def pay_calc(hours, rate) :
+    # if the user inputs hours over 40 we follow this if path
     if hours >40 :
+        # var reg is equal to rate • hours (both input by user)
         reg = rate * hours
-        otp = (hours - 40) * (rate * 0.5)
+        #  this calculates overtime pay (represented as var otp),
+        otp = (hours - 40) * (rate * 1.5)
+        # x represents our total, regular pay and overtime pay
         x = reg + otp
+        # returns the string 'Pay' and the value of variable x
         print('Pay', x)
+    # for cases where the hours are under 40 or equal to 40, everything but >40
     else :
+        # variable x will hold the value of regular pay
         x = reg
         print('Pay', x)
+# ?????????????????????????
     return x
+# ?????????????????????????
 
+# a try loop to handle the user inputs
 try :
     hrs = float(input('hours: '))
     r = float(input('hourly rate: '))
 except :
     print('numbers only')
 
-    
+# calls our defined function pay_calc (above^)
 pay = pay_calc(hrs,r)
