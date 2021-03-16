5e6e2e413745b7d17896e5c2c934b9c73620e6aa
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-16 13:26:44 -0700

    Update PY4E_11a.py

--- py4e_11/PY4E_11a.py
+++ py4e_11/PY4E_11a.py
@@ -2,14 +2,28 @@
 
 import re
 
-file_inp = import('ENTER FILE NAME >>> ')
+file_inp = input('ENTER FILE NAME >>> ')
 f_handl = open(file_inp)
-usr_srch = import('SEARCH REGEX >>> ')
 
 count = 0
-for line in f_handl :
-    line = line.rstrip()
-    if re.search(usr_srch, line) :
-        count += 1
-        print(line)
+usr_srch = input('SEARCH REGEX >>> ')
+if usr_srch.endswith(' ') :
+    usr_a = input('DID YOU MEAN TO END W WHITESPACE ??? (y for YES, n for NO) >>> ')
+    if usr_a == 'y' :
+        usr_str = str(usr_srch)
+        for line in f_handl :
+            if re.search(usr_str, line) :
+                count += 1
+                print(line)
+    elif usr_a == 'n' :
+        usr_str = str(usr_srch)
+        usr_clean = usr_str.rstrip()
+        for line in f_handl :
+            if re.search(usr_clean, line) :
+                count += 1
+                print(line)
+    else :
+        print('BOO ERROR')
+
+
 print('%s LINES MATCHING REGEX' % count)
