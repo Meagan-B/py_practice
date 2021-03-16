8c5087c9e8e655639cf2afb0be820a216f46a1f2
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-16 13:26:52 -0700

    Create py4e_11b.py

---
+++ py4e_11/py4e_11b.py
@@ -0,0 +1,20 @@
+# The basic outline of this problem is to read the file, look for integers using the re.findall(), looking for a regular expression of '[0-9]+' and then converting the extracted strings to integers and summing up the integers.
+#
+# >>>>>>>>>>>>>>
+import re
+
+
+file_inp = input('ENTER FILE NAME >>> ')
+f_handl = open(file_inp)
+
+
+num_collect = []
+for line in f_handl :
+    # line = line.rstrip()
+    if re.search('\s([0-9]+)', line) is not None :
+        num_find = re.findall('\s([0-9]+)', line)
+        if len(num_find) > 0 :
+            print(num_find)
+            # num_collect.append(int(num_find))
+
+print(num_collect)
