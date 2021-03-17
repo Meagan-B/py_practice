0c827976a8a1b44e81f261d2d15aa5b9d860d4ae
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-17 13:40:36 -0700

    Update py4e_11b.py
    curently extracts numbers from file and converts to float. still need to check for accuracy (missing digits), also need to collect all numbers in single place and do calculations.

--- py4e_11/py4e_11b.py
+++ py4e_11/py4e_11b.py
@@ -13,18 +13,11 @@ for line in f_handl :
     line = line.rstrip()
     line = line.split()
     for items in line :
-        if len(items) == 0 : continue
         num_find = re.findall('\d+', items)
-        print(num_find)
         for i in num_find :
-            print(i)
-        # if len(num_find) == 1 :
-        #     print('len=1', num_find)
-        #     print(type(num_find))
-            # num = str(num_find)
-            # num = float(num_find)
-            # print(num)
-            # num_collect.append(num)
+            if len(items) != 0 or len(items) is not None :
+                num = float(i)
+                # num_collect.append(num)
         # elif len(num_find) > 1 :
         #     print('Len=2', num_find)
         #     num_splt = item.split()
