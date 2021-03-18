6c6f184ef348ca8cee0a5095431f75af86498343
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-18 12:21:33 -0700

    Update PY4E_11a.py
    additional steps added to finishes chapter 11 exercises

--- py4e_11/PY4E_11a.py
+++ py4e_11/PY4E_11a.py
@@ -1,4 +1,6 @@
 # Exercise 1: Write a simple program to simulate the operation of the grep command on Unix. Ask the user to enter a regular expression and count the number of lines that matched the regular expression:
+# >>>>>>>>>>>>>>>>>>>>>>>
+
 
 import re
 
@@ -27,3 +29,21 @@ if usr_srch.endswith(' ') :
 
 
 print('%s LINES MATCHING REGEX' % count)
+
+# >>>>>>>>>>>>>>>>>>>
+# Exercise 2: Write a program to look for lines of the form:
+#
+# New Revision: 39772
+# >>>>>>>>>>>>>>>>>>>>
+
+
+
+# >>>>>>>>>>>>>>>>>>>
+# Extract the number from each of the lines using a regular expression and the findall() method. Compute the average of the numbers and print out the average as an integer.
+#
+# Enter file:mbox.txt
+# 38549
+#
+# Enter file:mbox-short.txt
+# 39756
+# >>>>>>>>>>>>>>>>>>
