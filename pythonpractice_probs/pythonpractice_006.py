6b20234f8cf52790d3d186b6b9bf9166610e4dbb
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-19 16:56:46 -0700

    Create pythonpractice_006.py

---
+++ pythonpractice_probs/pythonpractice_006.py
@@ -0,0 +1,12 @@
+# Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+#
+# >>>>>>>>>>>>>>>>>>>>>
+# WIP
+
+usr_inp = input('enter STRING >>> ')
+print('checking for palindromes……')
+usr_inp = usr_inp.split()
+print(usr_inp)
+
+
+# if usr_inp[0] == usr_inp[-1] :
