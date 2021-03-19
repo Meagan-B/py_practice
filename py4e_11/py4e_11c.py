85d78c7114b925bb6cb90ca0b4c83fd2d68907fe
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-18 21:24:32 -0700

    Update py4e_11c.py
    not working, taking a break on this one

--- py4e_11/py4e_11c.py
+++ py4e_11/py4e_11c.py
@@ -1,16 +1,31 @@
-# >>>>>>>>>>>>>>>>>>>
 # Exercise 2: Write a program to look for lines of the form:
 #
 # New Revision: 39772
 # >>>>>>>>>>>>>>>>>>>>
 
+
 import re
 
+usr_file = input('ENTER FILE NAME >>> ')
+f_handl = open(usr_file)
+r_file = f_handl.read()
 
-print([str(i) for i in re.findall('New\s\w:\s\d+', open(input('ENTER FILE NAME >>> ')).read())])
 
+lst_collect = []
+# num_find = re.findall('\<New\> \<Revision\>: \d+', f_handl)
+# lst_collect.append(num_find)
 
+for line in r_file :
+    line = line.rstrip()
+    line_find = re.search('New.+: \d+', line)
+    ## usr_srch = input('SEARCH REGEX >>> ')
+    ## usr_srch = re.search(usr_srch, line)
+    ## usr_srch = re.search(input('SEARCH REGEX >>> '), line)
+    ## lst_collect.append(usr_srch)
+    ### print(num_find = re.search('New\s\w:\s\39772', line)
+    lst_collect.append(line_find)
 
+print(lst_collect)
 
 # >>>>>>>>>>>>>>>>>>>
 # Extract the number from each of the lines using a regular expression and the findall() method. Compute the average of the numbers and print out the average as an integer.
