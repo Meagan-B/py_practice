a42135d7d107f05f738941fd4a43baab488f8537
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-19 18:41:59 -0700

    Update pythonpractice_006.py
    working, created a function to compare lists, but unsure if there is a value in using a function here

--- pythonpractice_probs/pythonpractice_006.py
+++ pythonpractice_probs/pythonpractice_006.py
@@ -5,28 +5,20 @@
 
 usr_word = input('enter STRING >>> ')
 print('checking for palindromes……')
-
-
-# def palind_fun(string) :
-#     usr_chars = list(usr_word.rstrip())
-#     if usr_chars[0] != usr_chars[-1] :
-#         print('……%s is NOT a palindrome!' % usr_word)
-#     elif usr_chars[0] == usr_chars[-1] :
-#         chars_reverse = usr_chars.reverse()
-#         if usr_chars == chars_reverse :
-#             print('……%s is a palindrome!' % usr_word)
-#     else :
-#         print('OoPs!')
-#
-# palind_fun(usr_word)
-
-
 usr_chars = list(usr_word.rstrip())
-if usr_chars[0] != usr_chars[-1] :
-    print('……%s is NOT a palindrome!' % usr_word)
-elif usr_chars[0] == usr_chars[-1] :
-    chars_reverse = usr_chars.reverse()
-    if usr_chars == chars_reverse :
-        print('……%s is a palindrome!' % usr_word)
+lst_rv = usr_chars[::-1]
+
+
+if usr_chars == lst_rv :
+    print('……%s is a palindrome!' % usr_word)
 else :
-    print('OoPs!')
+    print('……better luck next time :(')
+
+    
+# def check_pdrone() :
+#     if usr_chars == lst_rv :
+#         print('……%s is a palindrome!' % usr_word)
+#     else :
+#         print('……better luck next time :(')
+#
+# check_pdrone()
