0dace323a9613bf8a0de3e99a970f8fa048f6828
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-22 15:45:35 -0700

    Update pythonpractice_008.py
    working on loop and user exit

--- pythonpractice_probs/pythonpractice_008.py
+++ pythonpractice_probs/pythonpractice_008.py
@@ -12,9 +12,12 @@ player_two = input('enter name for player two >>> ')
 
 # ----
 
-print('~ HOW TO PLAY ~\n using numbers (1,2 or 3), make a play (ROCK, PAPER or SCISSORS)\n ROCK beats SCISSORS\n SCISSORS beats PAPER\n PAPER beats ROCK\n ~ GAME CONTROLS ~\n 1) ROCK\n 2) PAPER\n 3) SCISSORS\n enter x to exit program' )
+print('~ HOW TO PLAY ~\n using numbers (1,2 or 3), make a play (ROCK, PAPER or SCISSORS)\n ROCK beats SCISSORS\n SCISSORS beats PAPER\n PAPER beats ROCK\n ~ GAME CONTROLS ~\n 1) ROCK\n 2) PAPER\n 3) SCISSORS' )
 
 # ----
+
+p1_score = []
+p2_score = []
 
 def play_compare() :
     p1 = input('%s enter your play……\n' % player_one)
@@ -23,24 +26,35 @@ def play_compare() :
         print("tie, there is NO winner")
     elif p1 == 1 and p2 == 2:
         print('%s is the winner!' % p2)
+        p2_score += 1
     elif p1 == 1 and p2 == 3:
         print('%s is the winner!' % p1)
+        p1_score += 1
     elif p1 == 2 and p2 == 3:
         print('%s is the winner!' % p2)
+        p2_score += 1
     elif p1 == 2 and p2 == 1:
-        print('%s is the winner!' % p2)
+        print('%s is the winner!' % p1)
+        p1_score += 1
     elif p1 == 3 and p2 == 2:
         print('%s is the winner!' % p1)
+        p1_score += 1
     elif p1 == 3 and p2 == 1:
         print('%s is the winner!' % p2)
+        p2_score += 1
     elif p1 > 3 or p1 < 1 or p2 > 3 or p2 < 1 :
         print('invalid input')
 
+play_compare()
+print(p1_score)
 # ----
 
-while p1 != 'x' or p2 != 'x' :
-    play_compare()
-
+oneup = input('would you like to play again?\n enter Y for yes\n enter N for no')
+while True :
+    if oneup == 'Y' :
+        play_compare()
+    elif oneup == 'N' :
+        Print('~ FINAL SCORES ~\n %s had %s wins\n \n %s had %s wins\n thank you for playing!' % player_one, , player_two,)
 # ----
 
 
