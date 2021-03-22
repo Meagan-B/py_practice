4a1f9ad0b85a0fe477cba225add20648e8940806
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-22 09:55:29 -0700

    Update pythonpractice_005.py
    working on prompt to match randomly generated integers

--- pythonpractice_probs/pythonpractice_005.py
+++ pythonpractice_probs/pythonpractice_005.py
@@ -10,31 +10,44 @@
 # Randomly generate two lists to test this
 # Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
 #
-# >>>>>>>>>>>>>>>>>>>>>
-# WIP
+#>>>>>>>>>>>>>>>>>>>>>
+import random
+
+#----
+
 print('SET MATCH, checks for element matches, does not count duplicates……')
 
+#----
 
-lst_a = list(input('enter items for set A >>> '))
+# lst_a = list(input('enter items for set A >>> '))
+# lst_b = list(input('enter items for set B >>> '))
 # lst_a = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89}
-lst_b = list(input('enter items for set B >>> '))
 # lst_b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
-set_a = set((lst_a))
-set_b = set((lst_b))
-print(type(set_a))
-c = set_a.intersection(set_b)
+# set_a = set((lst_a))
+# set_b = set((lst_b))
+# print(type(set_a))
+# c = set_a.intersection(set_b)
 # print(set_a.intersection(set_b))
-print(type(c), c)
+
+#----
+
+rand_a = set()
+while len(rand_la) <10 :
+    val = randint(0,100)
+    rand_a.append(val)
+print(rand_a)    
+# rand_b = set()
+# while len(rand_b) <10 :
+#     val = randint(0,100)
+#     rand_b.append(val)
 
 
-# for i in c_lst :
-#     if i in a_lst == i in b_lst :
-#         print(i)
-#         e_lst.append(i)
-#         print(e_lst)
-#     elif item in b_lst == item in a_lst and item not in e_lst :
-#         print(item)
-#         e_lst.append(item)
-#         break
+# set_a = set((lst_a))
+# set_b = set((lst_b))
+# print(type(set_a))
+# c = set_a.intersection(set_b)
+
+
+
 #
 print('end')
