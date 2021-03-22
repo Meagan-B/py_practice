5309f48e60c06db5cc2c807c46ef91d2196ed899
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-22 12:01:33 -0700

    Update pythonpractice_004.py
    notes

--- pythonpractice_probs/pythonpractice_004.py
+++ pythonpractice_probs/pythonpractice_004.py
@@ -1,13 +1,21 @@
 # Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
 # For example, 13 is a divisor of 26 because 26 / 13 has no remainder.
+# >>>>>>>>>>>>>>>>>>>>>>
 
 usr_num = int(input('enter a number for a list of its divisors, from 1-100. >> '))
 divisors = list(range(1, 1000001))
 b = []
+
+# ----
 
 for i in divisors :
     if usr_num % i == 0 :
         b.append(i)
     else :
         continue
+
+# ----
+
 print(b)
+
+# ......................
