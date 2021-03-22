cca794fd6aa7bb7d610e52e8bca248c10568b55c
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-22 13:39:22 -0700

    Create pythonpractice_008.py

---
+++ pythonpractice_probs/pythonpractice_008.py
@@ -0,0 +1,23 @@
+# Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
+#
+# Remember the rules:
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
+#
+# >>>>>>>>>>>>>>>>>>>>>
+
+player_one = input('enter name for player one')
+# player_two = input('enter name for player two')
+
+# ----
+
+# round_1_a = input
+round_1_b = input('%s enter play\n……' % , '1) rock', \n, '2) paper', \n '3) scissors')
+# ----
+
+
+# ----
+
+
+# ...........................
