65908764aed9bfa496d212f7ddd0236917be461d
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-22 13:19:52 -0700

    Update pythonpractice_007.py
    final notes

--- pythonpractice_probs/pythonpractice_007.py
+++ pythonpractice_probs/pythonpractice_007.py
@@ -4,10 +4,8 @@
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 # ----
-
-a_even = [i in a if i / 2 == % 0]
+# variable a_even is equal to the argument within [], create an an item(i) from item(i) in list(a) ONLY IF item(i) divided by 2 has a remainder(%) of 0,
+a_even = [i for i in a if i % 2 == 0]
 print(a_even)
-
-
 
 # ...........................
