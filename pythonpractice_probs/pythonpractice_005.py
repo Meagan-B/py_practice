844c3126c926e58b16d39a01c7378e85f121b31a
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-22 10:58:43 -0700

    Update pythonpractice_005.py
    random list generation working, now working on notes

--- pythonpractice_probs/pythonpractice_005.py
+++ pythonpractice_probs/pythonpractice_005.py
@@ -18,9 +18,8 @@ import random
 print('SET MATCH, checks for element matches, does not count duplicates……')
 
 #----
+# comparison with a previously determined list
 
-# lst_a = list(input('enter items for set A >>> '))
-# lst_b = list(input('enter items for set B >>> '))
 # lst_a = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89}
 # lst_b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
 # set_a = set((lst_a))
@@ -30,25 +29,42 @@ print('SET MATCH, checks for element matches, does not count duplicates……')
 # print(set_a.intersection(set_b))
 
 #----
+# comparison with an imported user string, converted to list, via list()
 
-rand_a = set()
-while len(rand_a) <10 :
-    for i in range(100) :
-        val = randint(0,100)
-        rand_a.append(val)
-print(rand_a)
-# rand_b = set()
-# while len(rand_b) <10 :
-#     val = randint(0,100)
-#     rand_b.append(val)
-
-
+# lst_a = list(input('enter items for set A >>> '))
+# lst_b = list(input('enter items for set B >>> '))
 # set_a = set((lst_a))
 # set_b = set((lst_b))
 # print(type(set_a))
 # c = set_a.intersection(set_b)
+# print(set_a.intersection(set_b))
+
+#----
+
+rand_a = []
+while len(rand_a) <10 :
+    val = random.randint(0,100)
+    rand_a.append(val)
 
 
+rand_b = []
+while len(rand_b) <10 :
+    val = random.randint(0,100)
+    rand_b.append(val)
 
-#
+#----
+
+set_a = set((rand_a))
+set_b = set((rand_b))
+print('set A : %s' % set_a, 'set B : %s' % set_b)
+
+#----
+
+c = set_a.intersection(set_b)
+print('%s MATCHES found……' % len(c), c)
+
+#----
+
 print('end')
+
+#........................
