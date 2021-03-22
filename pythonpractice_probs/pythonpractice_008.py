3f49fac157a6dbbc0526c6c494b2a83528e82a01
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-22 15:29:08 -0700

    Update pythonpractice_008.py
    trying to house game into function and house function call in loop to continue game

--- pythonpractice_probs/pythonpractice_008.py
+++ pythonpractice_probs/pythonpractice_008.py
@@ -11,12 +11,35 @@ player_one = input('enter name for player one >>> ')
 player_two = input('enter name for player two >>> ')
 
 # ----
-print('~ HOW TO PLAY ~\n using numbers (1,2 or 3), make a play (ROCK, PAPER or SCISSORS)\n ROCK beats SCISSORS\n SCISSORS beats PAPER\n PAPER beats ROCK\n ~ GAME CONTROLS ~\n 1) ROCK\n 2) PAPER\n 3) SCISSORS')
 
-round_1_a = input('%s enter your play……\n' % player_one)
-round_1_b = input('%s enter your play……\n' % player_two)
+print('~ HOW TO PLAY ~\n using numbers (1,2 or 3), make a play (ROCK, PAPER or SCISSORS)\n ROCK beats SCISSORS\n SCISSORS beats PAPER\n PAPER beats ROCK\n ~ GAME CONTROLS ~\n 1) ROCK\n 2) PAPER\n 3) SCISSORS\n enter x to exit program' )
+
 # ----
 
+def play_compare() :
+    p1 = input('%s enter your play……\n' % player_one)
+    p2 = input('%s enter your play……\n' % player_two)
+    if p1 == p2:
+        print("tie, there is NO winner")
+    elif p1 == 1 and p2 == 2:
+        print('%s is the winner!' % p2)
+    elif p1 == 1 and p2 == 3:
+        print('%s is the winner!' % p1)
+    elif p1 == 2 and p2 == 3:
+        print('%s is the winner!' % p2)
+    elif p1 == 2 and p2 == 1:
+        print('%s is the winner!' % p2)
+    elif p1 == 3 and p2 == 2:
+        print('%s is the winner!' % p1)
+    elif p1 == 3 and p2 == 1:
+        print('%s is the winner!' % p2)
+    elif p1 > 3 or p1 < 1 or p2 > 3 or p2 < 1 :
+        print('invalid input')
+
+# ----
+
+while p1 != 'x' or p2 != 'x' :
+    play_compare()
 
 # ----
 
