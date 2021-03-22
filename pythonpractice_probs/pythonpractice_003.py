b6555b5696b35c2962b546dca0d2100ebc2c5960
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-22 12:01:30 -0700

    Update pythonpractice_003.py
    notes

--- pythonpractice_probs/pythonpractice_003.py
+++ pythonpractice_probs/pythonpractice_003.py
@@ -8,10 +8,12 @@
 # Write this in one line of Python.
 #
 # Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
-
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 a = [1, 3, 2, 4, 19, 23, 278, 10, 736, 0.5]
 b = []
+
+# ----
 
 for num in a :
     if num < 5 :
@@ -19,6 +21,10 @@ for num in a :
         # print(b)
     else :
         continue
+
+# ----
 print(b)
 b.sort()
 print(b)
+
+# ......................
