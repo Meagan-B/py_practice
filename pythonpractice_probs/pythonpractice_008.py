b7ad36fe51b8e35cab4d373b66ba19af11bcb7ef
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-23 12:59:52 -0700

    Update pythonpractice_008.py
    removed function, because it really served no valid purpose her and was disfunctional

--- pythonpractice_probs/pythonpractice_008.py
+++ pythonpractice_probs/pythonpractice_008.py
@@ -13,7 +13,7 @@ player_two = input('enter name for player two >>> ')
 
 # ----
 
-print('~ HOW TO PLAY ~\n using numbers (1,2 or 3), make a play (ROCK, PAPER or SCISSORS)\n ROCK beats SCISSORS\n SCISSORS beats PAPER\n PAPER beats ROCK\n' )
+print('~ HOW TO PLAY ~\nmake a play by entering "rock", "paper" or "scissors"\n ROCK beats SCISSORS\n SCISSORS beats PAPER\n PAPER beats ROCK\n')
 
 # ----
 
@@ -21,44 +21,54 @@ p1_score = []
 p2_score = []
 rps_game_dict = {'rock':1, 'paper':2, 'scissors':3}
 
+# ----
 
-def play_compare() :
-    p1 = input('%s enter your play……\n (rock, paper or scissors)' % player_one)
-    p2 = input('%s enter your play……\n (rock, paper or scissors)' % player_two)
-    global p1_score
-    global p2_score
-    game = p1 - p2
+while True :
+    p1 = str(input('%s enter your play……\n(rock, paper or scissors) >>> ' % player_one))
+    p2 = str(input('%s enter your play……\n(rock, paper or scissors) >>> ' % player_two))
+    p1_play = rps_game_dict.get(p1)
+    print(p1_play)
+    p2_play = rps_game_dict.get(p2)
+    game = p1_play - p2_play
+
     if game == 0 :
         print("tie, there is NO winner")
+        if str(input('would you like to play again?\n enter Y for yes\n enter N for no')) == Y : continue
+        else :
+            break
     elif game == 1 :
         print('%s is the winner!' % player_one)
         p2_score += 1
+        if str(input('would you like to play again?\n enter Y for yes\n enter N for no')) == Y : continue
+        else :
+            break
     elif game == -1 :
         print('%s is the winner!' % player_two)
         p1_score += 1
+        if str(input('would you like to play again?\n enter Y for yes\n enter N for no')) == Y : continue
+        else :
+            break
     elif game == 2 :
         print('%s is the winner!' % player_two)
         p2_score += 1
+        if str(input('would you like to play again?\n enter Y for yes\n enter N for no')) == Y : continue
+        else :
+            break
     elif game == -2 :
         print('%s is the winner!' % player_one)
         p1_score += 1
+        if str(input('would you like to play again?\n enter Y for yes\n enter N for no')) == Y : continue
+        else :
+            break
     elif game <= -3 or game >= 3 :
         print('invalid input')
-    # else :
-        # print('error')
+        if str(input('would you like to play again?\n enter Y for yes\n enter N for no')) == Y : continue
+        else :
+            break
 
-# p1_score = dict()
-# p2_score = dict()
-play_compare()
 # ----
 
 
-oneup = input('would you like to play again?\n enter Y for yes\n enter N for no')
-while True :
-    if oneup == 'Y' :
-        play_compare()
-    # elif oneup == 'N' :
-    #     Print('~ FINAL SCORES ~\n %s had %s wins\n \n %s had %s wins\n thank you for playing!' % player_one, , player_two,)
 # ----
 
 
