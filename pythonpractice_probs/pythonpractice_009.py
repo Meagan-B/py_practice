583d1960056f5694d2e335e2de38aa3644473b0c
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-24 21:23:31 -0700

    Create pythonpractice_009.py

---
+++ pythonpractice_probs/pythonpractice_009.py
@@ -0,0 +1,38 @@
+# Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right. (Hint: remember to use the user input lessons from the very first exercise)
+#
+# Extras:
+#
+# Keep the game going until the user types “exit”
+# Keep track of how many guesses the user has taken, and when the game ends, print this out.
+#
+# >>>>>>>>>>>>>>>>>>>>>
+
+import random
+
+# ----
+
+# print('\n~~~~~~~~ HOW TO PLAY ~~~~~~~~\n\n\n')
+
+# ----
+
+# = input('')
+
+# ----
+
+# p_score = 0
+
+# ----
+
+while True :
+    print('\n•••••••• NEW GAME ••••••••\n')
+
+    # if game == 0 :
+    #     print('\n<<<< DRAW, there is NO winner >>>>')
+    #     if input('\nwould you like to play again?\n"Y" for yes\nenter to QUIT\n*CASE SENSITIVE*\n >>> ') == 'Y' : continue
+    #     else :
+    #         break
+# ----
+
+print('\nxxXXXXxx GAME OVER xxXXXXxx')
+
+# ...........................
