ae06d9bb7227b06383e1390d1b9395e66e1a61ec
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-28 14:43:48 -0700

    Update pythonpractice_011.py

--- pythonpractice_probs/pythonpractice_011.py
+++ pythonpractice_probs/pythonpractice_011.py
@@ -2,60 +2,53 @@
 #
 # >>>>>>>>>>>>>>>>>>>>>
 
-import re
-
-# ----
-
-usr_n_str = input('\nenter a number below to see if it is a PRIME NUMBER……\n**** a prime number is a number with no divisors ****\n>>> ')
-usr_n = int(usr_n_str)
-
-# ----
-
-ints = [2, 3, 4, 5, 6, 8, 9]
-last_dig = usr_n % 10
-
-
-if len(usr_n_str) > 1 :
-    # print(type(last_dig))
-    print(last_dig)
-    for i in ints :
-        if last_dig == i :
-            # print('BOB1')
-            print('\n{0} is NOT a prime number\n'.format(usr_n))
-        else : continue
-
-if usr_n <= 1 :
-    # print('BOB2')
-    print('\n{0} is NOT a prime number\n'.format(usr_n))
-
-for n in range(2, usr_n) :
-    if usr_n % n == 0 :
-        # print('BOB3')
-        print('\n{0} is NOT a prime number\n'.format(usr_n))
-    else : continue
-
-print('out of loop')
-
 
 # ----
 
 def prime_calc(n) :
-    if len(usr_n) > 1 and re.search('$[02345689]', usr_n)  :
-        print('\n{0} is NOT a prime number\n'.format(n))
+    usr_n_str = input('\nenter a number below to see if it is a PRIME NUMBER……\n**** a prime number is a number with no divisors ****\n>>> ')
+    ints = [2, 3, 4, 5, 6, 8, 9]
+
+    usr_n = int(usr_n_str)
+    last_dig = usr_n % 10
+
+    if len(usr_n_str) > 1 :
+        for i in ints :
+            if last_dig == i :
+                print('\n{0} is NOT a prime number\n'.format(usr_n))
+                return False
+
+    if usr_n <= 1 :
+        print('\n{0} is NOT a prime number\n'.format(usr_n))
         return False
 
-    n = int(n)
-    if n <= 1 :
-        print('\n{0} is NOT a prime number\n'.format(n))
-        return False
-
-    for i in range(2, n) :
-        if usr_n % i == 0 :
-            print('\n{0} is NOT a prime number\n'.format(n))
+    for n in range(2, usr_n) :
+        if usr_n % n == 0 :
+            print('\n{0} is NOT a prime number\n'.format(usr_n))
             return False
         else :
             print('\n{0} IS a prime number\n'.format(n))
             return True
+
+# ----
+#
+# def prime_calc(n) :
+#     if len(usr_n) > 1 and re.search('$[02345689]', usr_n)  :
+#         print('\n{0} is NOT a prime number\n'.format(n))
+#         return False
+#
+#     n = int(n)
+#     if n <= 1 :
+#         print('\n{0} is NOT a prime number\n'.format(n))
+#         return False
+#
+#     for i in range(2, n) :
+#         if usr_n % i == 0 :
+#             print('\n{0} is NOT a prime number\n'.format(n))
+#             return False
+#         else :
+#             print('\n{0} IS a prime number\n'.format(n))
+#             return True
 # ----
 
 # prime_calc(usr_n)
