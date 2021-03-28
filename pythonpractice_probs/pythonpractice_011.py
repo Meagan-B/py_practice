3d81e4c1680b0ed7273a0287e85f2f91d1ecb1b2
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-28 15:00:08 -0700

    Update pythonpractice_011.py

--- pythonpractice_probs/pythonpractice_011.py
+++ pythonpractice_probs/pythonpractice_011.py
@@ -2,55 +2,44 @@
 #
 # >>>>>>>>>>>>>>>>>>>>>
 
+usr_n_str = input('\nenter a number below to see if it is a PRIME NUMBER……\n**** a prime number is a number with no divisors ****\n>>> ')
 
 # ----
 
 def prime_calc(n) :
-    usr_n_str = input('\nenter a number below to see if it is a PRIME NUMBER……\n**** a prime number is a number with no divisors ****\n>>> ')
-    ints = [2, 3, 4, 5, 6, 8, 9]
+    global usr_n_str
+    end_ints = [2, 3, 4, 5, 6, 8, 9]
 
     usr_n = int(usr_n_str)
     last_dig = usr_n % 10
 
-    if len(usr_n_str) > 1 :
-        for i in ints :
-            if last_dig == i :
-                print('\n{0} is NOT a prime number\n'.format(usr_n))
-                return False
+    for i in end_ints :
+        if i == usr_n :
+            return False
 
     if usr_n <= 1 :
-        print('\n{0} is NOT a prime number\n'.format(usr_n))
+        # print('exit 1')
         return False
+
+    elif len(usr_n_str) > 1 :
+        for i in end_ints :
+            if last_dig == i :
+                # print('exit 2')
+                return False
 
     for n in range(2, usr_n) :
         if usr_n % n == 0 :
-            print('\n{0} is NOT a prime number\n'.format(usr_n))
+        # print('exit 3')
             return False
         else :
-            print('\n{0} IS a prime number\n'.format(n))
+            # print('exit 4')
             return True
 
 # ----
-#
-# def prime_calc(n) :
-#     if len(usr_n) > 1 and re.search('$[02345689]', usr_n)  :
-#         print('\n{0} is NOT a prime number\n'.format(n))
-#         return False
-#
-#     n = int(n)
-#     if n <= 1 :
-#         print('\n{0} is NOT a prime number\n'.format(n))
-#         return False
-#
-#     for i in range(2, n) :
-#         if usr_n % i == 0 :
-#             print('\n{0} is NOT a prime number\n'.format(n))
-#             return False
-#         else :
-#             print('\n{0} IS a prime number\n'.format(n))
-#             return True
-# ----
 
-# prime_calc(usr_n)
+if prime_calc(usr_n_str) == False :
+     print('\n{0} is NOT a prime number\n'.format(usr_n_str))
+elif prime_calc(usr_n_str) == True :
+     print('\n{0} IS a prime number\n'.format(usr_n_str))
 
 # ...........................
