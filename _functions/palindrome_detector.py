48512d27006726483082ed574155621eea1b1fed
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-31 19:37:06 -0700

    Update palindrome_detector.py
    function working

--- _functions/palindrome_detector.py
+++ _functions/palindrome_detector.py
@@ -1,32 +1,20 @@
 # Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
 #
 # >>>>>>>>>>>>>>>>>>>>>
-# WIP
-# MAKE INTO FUNCTION
 
-usr_word = input('enter STRING >>> ')
-print('checking for palindromes……')
+def check_pdrone() :
 
-# ----
+    usr_word = input('enter STRING below\n>>> ')
+    usr_chars = list(usr_word.rstrip())
+    lst_rv = usr_chars[::-1]
 
-usr_chars = list(usr_word.rstrip())
-lst_rv = usr_chars[::-1]
-
-# ----
-
-if usr_chars == lst_rv :
-    print('……%s is a palindrome!' % usr_word)
-else :
-    print('……better luck next time :(')
+    if usr_chars == lst_rv :
+        print('……{0} IS a palindrome'.format(usr_word))
+    else :
+        print('……{0} is NOT a palindrome'.format(usr_word))
 
 # ----
 
-# def check_pdrone() :
-#     if usr_chars == lst_rv :
-#         print('……%s is a palindrome!' % usr_word)
-#     else :
-#         print('……better luck next time :(')
-#
-# check_pdrone()
+check_pdrone()
 
 # ...........................
