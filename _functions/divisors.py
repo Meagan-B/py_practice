5e3e8fa3b5142ab0358f5b805d74cc43ab8a808c
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-03-31 19:23:25 -0700

    Update divisors.py
    function now working

--- _functions/divisors.py
+++ _functions/divisors.py
@@ -1,23 +1,22 @@
 # Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
 # For example, 13 is a divisor of 26 because 26 / 13 has no remainder.
 # >>>>>>>>>>>>>>>>>>>>>>
-# WIP
-# MAKE INTO FUNCTION
 
-usr_num = int(input('enter a number for a list of its divisors, from 1-100. >> '))
-divisors = list(range(1, 1000001))
-b = []
+def divisor_check() :
+    usr_num = int(input('enter a number for a list of its divisors\n>>> '))
+    divisors = list(range(1, usr_num))
+    b = []
+
+    for i in divisors :
+        if usr_num % i == 0 :
+            b.append(i)
+        else :
+            continue
+
+    print(b)
 
 # ----
 
-for i in divisors :
-    if usr_num % i == 0 :
-        b.append(i)
-    else :
-        continue
-
-# ----
-
-print(b)
+divisor_check()
 
 # ......................
