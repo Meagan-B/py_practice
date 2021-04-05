ef2208fde93aa6925938743761aca0b9a367ab85
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-05 15:14:50 -0700

    Update py4e_12a.py
    working on handling urls with and without 'http://" chars

--- py4e_12/py4e_12a.py
+++ py4e_12/py4e_12a.py
@@ -25,12 +25,21 @@ import socket
 
 usr_url = str(input('enter URL below\n>>> '))
 usr_url = usr_url.rstrip()
-usr_url = usr_url.split('/')
-print(type(usr_url), usr_url)
+
+if re.search('/+') :
+    # usr_url = usr_url.split('/')
+    print(usr_url)
+elif :
+    usr_url = usr_url.split()
+    print(usr_url)
+# print(type(usr_url), usr_url)
 
 for i in usr_url :
     if re.search('^.+\..+\.[a-z]+$', i) :
-        print(i)
+        usr_url = i
+        # print(usr_url)
+
+# ----
 
 
 # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
