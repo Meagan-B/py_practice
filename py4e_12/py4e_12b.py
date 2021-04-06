1c9f2a05bdff8eec433b5464c04b3d9e76a76988
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-06 16:04:07 -0700

    Update py4e_12b.py
    added time for future uses

--- py4e_12/py4e_12b.py
+++ py4e_12/py4e_12b.py
@@ -1,44 +1,50 @@
 # Exercise 2: Change your socket program so that it counts the number of characters it has received and stops displaying any text after it has shown 3000 characters. The program should retrieve the entire document and count the total number of characters and display the count of the number of characters at the end of the document.
 # >>>>>>>>>>>>>>>>>>>>>>>
-
 import re
 import socket
+# import time
 
 # ----
 
-usr_url = str(input('enter URL below\n>>> '))
-usr_url = usr_url.rstrip()
+usr_host = str(input('enter URL below\n>>> '))
+usr_host = usr_host.rstrip()
 
-if re.search('/+', usr_url) :
-    usr_url = usr_url.split('/')
+if re.search('/+', usr_host) :
+    usr_host = usr_host.split('/')
     # print(usr_url)
-    for i in usr_url :
+    for i in usr_host :
         if re.search('^.+\..+\.[a-z]+$', i) :
-            usr_url = i
+            usr_host = i
             # print(usr_url)
 
-print(type(usr_url), usr_url)
+# print(type(usr_host), usr_host)
 # ----
+data_collect = str()
 
 try :
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((usr_url, 80))
-    # h_proto = ('GET {0} HTTP/1.0\r\n\r\n'.format(usr_url)).encode()
+    sock.connect((usr_host, 80))
+    # h_proto = ('GET {0} HTTP/1.0\r\n\r\n'.format(usr_host)).encode()
     h_proto = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+    # h_proto = 'GET http://data.pr4e.org/ HTTP/1.0\r\n\r\n'.encode()
     sock.send(h_proto)
 
     while True :
-        data = sock.recv(512)
+        data = sock.recv(10)
+        # print(data)
+        # time.sleep(0.05)
         if len(data) < 1 :
             break
-        # print(data.decode(), end='')
+        print(data.decode())
+        # data_collect = data_collect.write(data.decode())
+
 except :
     print('OOPS, bad link')
 
 
-
-# ----
-
+# links = re.findall(b'href="(http[s]?://.*?)"', u_fhand)
+# for link in links :
+#     print(links.decode())
 
 # ----
 
