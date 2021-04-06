ed63c380dbe599ed99fbe05b3c08b5c11f855678
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-06 13:27:03 -0700

    Update py4e_12a.py
    missing port on socket connection (sock.connect((usr_url, X))

--- py4e_12/py4e_12a.py
+++ py4e_12/py4e_12a.py
@@ -38,7 +38,7 @@ print(type(usr_url), usr_url)
 # ----
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((usr_url))
+sock.connect((usr_url, 80))
 h_proto = 'GET {0} HTTP/1.0\n\n'.format(usr_url).encode()
 sock.send(h_proto)
 
