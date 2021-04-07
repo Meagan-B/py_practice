f99a0feb1daa1082dea794e88d9ef5b940d06d99
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-06 20:01:56 -0700

    Update py4e_12c.py
    trying to find a way to select a file from main page and opne page containing selected file

--- py4e_12/py4e_12c.py
+++ py4e_12/py4e_12c.py
@@ -13,9 +13,13 @@ usr_host = usr_host.rstrip()
 
 with urllib.request.urlopen(usr_host) as response :
     html = response.read()
+
     links = re.findall(b'href="(.*?\..*?)"', html)
     for link in links :
         print(link.decode())
+
+    link_pik = input('\r\n\r\nenter a link, from list above\n>>> ').urlopen(link_pik)
+
     # print(html)
 
 # ----
