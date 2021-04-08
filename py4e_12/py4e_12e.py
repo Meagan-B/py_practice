e1501f914a0daa4cbb6d72a916b7b3bb5cd5866b
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-08 16:34:03 -0700

    Update py4e_12e.py
    starting

--- py4e_12/py4e_12e.py
+++ py4e_12/py4e_12e.py
@@ -11,3 +11,55 @@
 # ----
 
 # ...........................
+#
+# >>>>>>>>>>>>>>>>>>>>>>>
+
+import re
+import socket
+
+# ----
+
+usr_host = str(input('enter URL below\n>>> '))
+usr_host = usr_host.rstrip()
+
+if re.search('/+', usr_host) :
+    usr_host = usr_host.split('/')
+    # print(usr_url)
+    for i in usr_host :
+        if re.search('^.+\..+\.[a-z]+$', i) :
+            usr_host = i
+            # print(usr_url)
+
+# print(type(usr_host), usr_host)
+# ----
+data_collect = str()
+
+try :
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((usr_host, 80))
+    # h_proto = ('GET {0} HTTP/1.0\r\n\r\n'.format(usr_host)).encode()
+    h_proto = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+    # h_proto = 'GET http://data.pr4e.org/ HTTP/1.0\r\n\r\n'.encode()
+    sock.send(h_proto)
+
+    while True :
+        data = sock.recv(512)
+        # print(data)
+        if len(data) < 1 :
+            break
+        print(data.decode())
+        # data_collect = data_collect.write(data.decode())
+
+except :
+    print('OOPS, bad link')
+
+
+# links = re.findall(b'href="(http[s]?://.*?)"', u_fhand)
+# for link in links :
+#     print(links.decode())
+
+# ----
+
+sock.close()
+
+# ...........................
