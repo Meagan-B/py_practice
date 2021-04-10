a84d66fdeeb678cfe6d959ae4f4a32c4f82c35dc
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-09 22:41:29 -0700

    Update py4e_12f.py
    still working on line 54/55

--- py4e_12/py4e_12f.py
+++ py4e_12/py4e_12f.py
@@ -52,7 +52,8 @@ soup = BeautifulSoup(html, 'html.parser')
 
 # num_count = 0
 
-for tag in soup.find_all(string=re.compile('comments=')) :
+# for tag in soup.find_all(id=re.compile('comments')) :
+for tag in soup.find_all(string=re.compile(('span'), class='comments')) :
     # '^<tr>.+\s.+>\d+<'
     # num_count += 1
     print('TAG:',tag)
