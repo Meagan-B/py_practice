8ed87a9783c1c2e4083d11c319f8f84aee3eef87
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-09 21:45:51 -0700

    Create py4e_12f.py
    working on auto grader 12.2

---
+++ py4e_12/py4e_12f.py
@@ -0,0 +1,65 @@
+# Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to
+#
+# http://www.py4e.com/code3/urllink2.py.
+#
+# The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+#
+# Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
+# Actual data: http://py4e-data.dr-chuck.net/comments_1154830.html (Sum ends with 77)
+#
+# >>>>>>>>>>>>>>>>>>>>>>>
+
+import re
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# ----
+# Ignore SSL certificate errors (from PY4E, because i do not understand, but need to use this code to test example)
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# ----
+
+usr_host = str(input('enter URL below\n>>> '))
+usr_host = usr_host.rstrip()
+
+# ----
+
+html = urllib.request.urlopen(usr_host, context=ctx).read()
+# print(html.decode())
+
+soup = BeautifulSoup(html, 'html.parser')
+# print(soup)
+
+# ----
+
+para_count = 0
+para_tag = soup('p')
+
+for tag in para_tag :
+    para_count += 1
+
+print('\n**** {0} paragraphs found ****\n'.format(para_count))
+
+# ----
+
+para_count = 0
+para_tag = soup('p')
+
+for tag in para_tag :
+   print 'TAG:',tag
+   print 'URL:',tag.get('href', None)
+   print 'Contents:',tag.contents[0]
+   print 'Attrs:',tag.attrs
+   # ^PY4E
+
+# ----
+
+# ----
+
+# ----
+
+# ...........................
