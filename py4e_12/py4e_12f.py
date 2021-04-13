a4073fadad4e76150cbc2f6caa6a118d0ac24807
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-13 09:46:57 -0700

    Update py4e_12f.py
    indentation error

--- py4e_12/py4e_12f.py
+++ py4e_12/py4e_12f.py
@@ -50,9 +50,9 @@ for tag in soup.find_all('span') :
     num = re.findall('\d+', span_tag)
     print(num)
     for n in num :
-    num_i = int(n)
-    print(n)
-    print(type(n))
+        num_i = int(n)
+        print(n)
+        print(type(n))
     # sum = sum(num)
     # print(sum)
     # print(sum(num))
