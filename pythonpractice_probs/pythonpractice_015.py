0ebb78b765b310d186ce571eeaff816f17fe8cea
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-20 13:05:52 -0700

    Update pythonpractice_015.py
    clean-up , with working code. still needs to be put in a function

--- pythonpractice_probs/pythonpractice_015.py
+++ pythonpractice_probs/pythonpractice_015.py
@@ -6,22 +6,25 @@
 usr_str = input('enter string below\n>>> ').strip()
 print(usr_str)
 
-usr_str = usr_str.split(' ')
+# ----
 
+usr_str = usr_str.split(' ')
 usr_str.reverse()
 
-usr_str = str(usr_str)
-print(usr_str)
+# ----
+
+rev_str = ''
+
+for i in usr_str :
+    rev_str += i
 
 # ----
 
-# def dup_cut_1() :
+print(rev_str)
+
+# ----
 
 
-# ----
-# ----
-# ----
-# ----
 
 
 
