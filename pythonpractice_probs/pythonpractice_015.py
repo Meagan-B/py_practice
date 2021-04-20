a461422b684a3cc762066f48427fa0ea454af484
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-20 12:49:50 -0700

    Update pythonpractice_015.py

--- pythonpractice_probs/pythonpractice_015.py
+++ pythonpractice_probs/pythonpractice_015.py
@@ -1,13 +1,13 @@
-# Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+# Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order
 # >>>>>>>>>>>>>>>>>>>>>
 
 # ----
 usr_str = input('enter string below\n>>> ').strip()
 print(usr_str)
 usr_str = usr_str.split(' ')
-print(usr_str)
 usr_str.reverse()
-print(usr_str)
+rev_str = str(usr_str)
+print(rev_str)
 
 # ----
 
