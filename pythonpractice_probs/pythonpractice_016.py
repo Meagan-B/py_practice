422b69f76f868044a9e652b172b45d2d00e35c43
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-26 11:02:10 -0700

    Update pythonpractice_016.py
    back on it after the clusterF that was the past two weeks,
    
    added imports for modules, random and string. using string.ascii to generate lists to pull from for password generator

--- pythonpractice_probs/pythonpractice_016.py
+++ pythonpractice_probs/pythonpractice_016.py
@@ -5,19 +5,31 @@
 # Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
 # >>>>>>>>>>>>>>>>>>>>>
 
-pass_rating = input('how strong of a password would you like?\nenter WEAK or STRONG below\n>>> ')
+import random
+import string
 
 # ----
-weak_pass =
-stong_pass =
+# pass_rating = input('how strong of a password would you like?\nenter WEAK or STRONG below\n>>> ')
+
+# ----
+
+alpha_lower = string.ascii_lowercase
+alpha_upper = string.ascii_uppercase
+
+print(alpha_lower)
+print(alpha_upper)
+
+# ----
+# weak_pass =
+# stong_pass =
 
 
 # ----
 
 # ----
-if pass_rating == 'WEAK' or pass_rating == 'weak' :
+# if pass_rating == 'WEAK' or pass_rating == 'weak' :
 
-elif pass_rating == 'STONG' or pass_rating 'strong' :
+# elif pass_rating == 'STONG' or pass_rating 'strong' :
 
 # ----
 
