d0f94632fe1fd75d4877af59a33d5298f9e61ec7
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-27 14:15:16 -0700

    Update pythonpractice_017.py
    good frame to start testing

--- pythonpractice_probs/pythonpractice_017.py
+++ pythonpractice_probs/pythonpractice_017.py
@@ -1,29 +1,23 @@
 # Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times homepage.
 # >>>>>>>>>>>>>>>>>>>>>
 
-import re
-import urllib.request, urllib.parse, urllib.error
+
+import requests
 from bs4 import BeautifulSoup
-import ssl
-
-# ----
-# Ignore SSL certificate errors (from PY4E, because i do not understand, but need to use this code to test example)
-
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
 
 # ----
 
-usr_host = str(input('enter URL below\n>>> '))
-usr_host = usr_host.rstrip()
+# usr_host = str(input('enter URL below\n>>> '))
+# usr_host = usr_host.rstrip()
+usr_host = 'http://www.nytimes.com'
 
 # ----
 
-html = urllib.request.urlopen(usr_host, context=ctx).read()
+html = request.get(usr_host)
 # print(html.decode())
+print(html)
 
-soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html.text)
 print(soup)
 
 # ----
@@ -37,7 +31,6 @@ print(soup)
 # # ----
 #
 # print('\n**** {0} paragraphs found ****\n'.format(para_count))
-
 
 
 
