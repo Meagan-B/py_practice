f3159df2f2fac94bd62351762e958765632890a8
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-30 12:24:27 -0700

    Create edabit_13.py

---
+++ edabit/edabit_13.py
@@ -0,0 +1,14 @@
+def sum_lst(n):
+    total = 0
+    n = n + 1
+
+    for i in range(0, n):
+        print(i)
+        total += i
+
+    print(total)
+    return total
+
+sum_lst(4)
+# sum_lst(8)
+# sum_lst(12)
