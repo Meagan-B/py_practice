5073931160b96a30de00f983f2cd3e71c6998a58
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-04-30 13:21:31 -0700

    Create edabit_16.py

---
+++ edabit/edabit_16.py
@@ -0,0 +1,11 @@
+string_1 = '2+2=4'
+string_2 = '2*3=6'
+string_3 = '10/2=6'
+string_4 = '30/15=2'
+
+def true_equations(a_string) :
+    a_string.split()
+    print(type(a_string), a_string)
+    return a_string
+
+true_equations(string_1)
