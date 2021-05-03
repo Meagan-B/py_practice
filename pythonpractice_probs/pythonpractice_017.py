d01a8ce5dbc3927fbdd2a669488c596a5e2c7cbc
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-05-03 14:47:46 -0700

    Update pythonpractice_017.py

--- pythonpractice_probs/pythonpractice_017.py
+++ pythonpractice_probs/pythonpractice_017.py
@@ -34,67 +34,11 @@ while count < 4 :
         else : continue
 
 print(link_collect)
-    # ----
 
-    # i = input('\nenter position\n>>> ')
-    # i = int(i) - 1
-    #
-    #
-    # next_link = link_collect[i]
-    # print('\nnext link to follow…\n{0}\n'.format(next_link))
-    # # usr_host = tag_collect[2]
-    # # print(usr_host)
-    # count += 1
-    #
-    # # ----
-    #
-    # cont = input('want to follow another link?\nY for yes, N for no\n>>> ')
-    #
-    # if cont == 'Y' or cont == 'y' : continue
-    # elif cont == 'N' or cont == 'n'  : break
-
-
-
-
-# # ----
-#
-# # usr_host = str(input('enter URL below\n>>> '))
-# # usr_host = usr_host.rstrip()
-# usr_host = 'http://www.nytimes.com'
-#
-# # ----
-#
-# html = requests.get(usr_host)
-# # print(html.decode())
-# # print(html)
-#
-# soup = BeautifulSoup(requests.get(usr_host).text, 'html.parser')
-# # soup = BeautifulSoup(usr_host.text, 'html.parser')
-# # print(soup)
-#
-# collection_pot = []
-# for story_head in soup.find_all(class_='balancedHeadline') :
-#     collection_pot += story_head
-#     # print(story_head)
-#
-# print(collection_pot)
-# # ----
-#
-# # <h3 class="css-svu3ba e1lsht870" size="500">Can You Have Alcohol After the Covid Vaccine?</h3>
-# # <h3 class="css-1bxzzgs e1lsht870" size="300">Why Iowa Has Become Such a Heartbreaker for Democrats</h3>
-#
-#
-# # para_count = 0
-# # para_tag = soup('p')
-# #
-# # for tag in para_tag :
-# #     para_count += 1
-# #
-# # # ----
-# #
-# # print('\n**** {0} paragraphs found ****\n'.format(para_count))
-
-
-
+# 
+# import urllib.request
+# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+# for line in fhand:
+#     print(line.decode().strip())
 
 # ...........................
