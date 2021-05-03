eaf47189d6d4be6205ac88b7c1addc9cbea85e43
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-05-03 15:28:05 -0700

    Update pythonpractice_017.py

--- pythonpractice_probs/pythonpractice_017.py
+++ pythonpractice_probs/pythonpractice_017.py
@@ -5,35 +5,51 @@ import re
 import urllib.request, urllib.parse, urllib.error
 import requests
 from bs4 import BeautifulSoup
+import ssl
 
-# usr_host = input('enter web address below\n>>> ')
-usr_host = 'http://www.nytimes.com'
-html = requests.get(usr_host)
-soup = BeautifulSoup(requests.get(usr_host).text, 'lxml')
+# ----
+# Ignore SSL certificate errors (from PY4E, because i do not understand, but need to use this code to test example)
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 # ----
 
-count = 0
-link_collect =[]
+# usr_host = input('enter web address below\n>>> ')
+# usr_host = 'http://www.nytimes.com'
+# html = requests.get(usr_host)
+# soup = BeautifulSoup(requests.get(usr_host).text, 'lxml')
+
+# ----
+
+fhand = urllib.request.urlopen('http://www.nytimes.com')
+for line in fhand:
+    print(line.decode().strip())
+# ----
+
+# count = 0
+# link_collect =[]
 
 # tags = soup.find_all('a', [class_="css-xxaj7r")
 
-while count < 4 :
-    tags = soup.find_all('a')
+# tags = soup.find_all('a')
+# print(tags)
     # tags = soup.find_all("a", class_="css-xxaj7r e1lsht870")
     # tags = soup.title
     # tags = soup.title.name
     # tags = soup.title.string
     # tags = soup.title.parent.name
     # tags = soup.p
-    for tag in tags :
-        if tag.re('class= css-xxaj7r') :
-            # link_collect.append(tag.get('href'))
-            link_collect.append(tag)
-            count += 1
-        else : continue
 
-print(link_collect)
+# for tag in tags :
+#     if tag.re('class= css-xxaj7r') :
+#         # link_collect.append(tag.get('href'))
+#         link_collect.append(tag)
+#         count += 1
+#     else : continue
+
+# print(link_collect)
 
 
 
