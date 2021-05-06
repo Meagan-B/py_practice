3873fa637c841f2f7a65720ea95cbee947fe14b1
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-05-06 14:20:00 -0700

    Update pythonpractice_018.py
    can now accurately detect if it is the same number

--- pythonpractice_probs/pythonpractice_018.py
+++ pythonpractice_probs/pythonpractice_018.py
@@ -8,32 +8,40 @@ how_to_play = 'For every correctly guessed digit in the CORRECT placement,\nyou 
 print(welcome + how_to_play)
 
 # ----
+
 import random
+
 # ----
+
 rand_num_gen = [random.randint(0, 9) for i in range(4)]
 num_strs = [str(n) for n in rand_num_gen]
 rand_str = int("".join(num_strs))
 
 print('* random number generated: {0} *'.format(rand_str))
-# ----
-u_guess = input('enter any 4 digit number\nmake thy first guess\n>>> ')
 
+# ----
+
+u_guess = int(input('enter any 4 digit number\nmake thy first guess\n>>> '))
 print('* user guess #1: {0} *'.format(u_guess))
+
 # ----
 
-guesses = 1
-cows = 0
-bulls = 0
+# guesses = 1
+# cows = 0
+# bulls = 0
 
-while True :
-    if u_guess == 'quit' : break
-    elif u_guess == rand_str :
-        cows = 4
-        print('you have {0} COWS!!!'.format(cows))
-        break
-    else :
-        print('* else *')
-        break
+print(u_guess == rand_str)
+
+
+# while True :
+#     print(u_guess, rand_str)
+#     if u_guess == 'quit' : break
+# elif u_guess == rand_str True :
+#         print('you have {0} COWS!!!'.format(4))
+#         break
+#     else :
+#         print('* else *')
+#         break
     # else :
     #     for i in u_guess :
     #     print(i)
@@ -49,7 +57,7 @@ while True :
     # guesses += 1
     # bulls += 1
     #
-    u_guess = input('guess again\n>>> ')
+    # u_guess = input('guess again\n>>> ')
 
 
 # ----
