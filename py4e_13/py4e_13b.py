52b6ab5e9c8182e373317c10d38a711912bc6f60
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-05-10 12:52:04 -0700

    Create py4e_13b.py

---
+++ py4e_13/py4e_13b.py
@@ -0,0 +1,46 @@
+# In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/json2.py. The program will prompt for a URL, read the JSON data from that URL using urllib and then parse and extract the comment counts from the JSON data, compute the sum of the numbers in the file and enter the sum below:
+#
+# We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+#
+# Sample data: http://py4e-data.dr-chuck.net/comments_42.json (Sum=2553)
+# Actual data: http://py4e-data.dr-chuck.net/comments_1154833.json (Sum ends with 33)
+#
+# You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.
+#
+# 
+# Enter location: http://py4e-data.dr-chuck.net/comments_42.json
+# Retrieving http://py4e-data.dr-chuck.net/comments_42.json
+# Retrieved 2733 characters
+# Count: 50
+# Sum: 2...
+# >>>>>>>>>>>>>>>>>>>>>>>
+
+# import urllib.request, urllib.parse, urllib.error
+# import xml.etree.ElementTree as ET
+# import ssl
+
+# ----
+# # Ignore SSL certificate errors (from PY4E, because i do not understand, but need to use this code to test example)
+#
+# ctx = ssl.create_default_context()
+# ctx.check_hostname = False
+# ctx.verify_mode = ssl.CERT_NONE
+
+# ----
+
+
+
+# ----
+
+# ----
+
+
+# ----
+
+
+# ----
+
+
+# ----
+
+# ...........................
