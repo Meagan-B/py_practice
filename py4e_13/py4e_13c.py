a750ea04129387119c34c6bc8d9e3312ea16d4cd
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-05-17 15:20:56 -0700

    Update py4e_13c.py
    no changes from py4e code yet

--- py4e_13/py4e_13c.py
+++ py4e_13/py4e_13c.py
@@ -1,57 +1,64 @@
 # Exercise 1: Change either geojson.py or geoxml.py to print out the two-character country code from the retrieved data. Add error checking so your program does not traceback if the country code is not there. Once you have it working, search for “Atlantic Ocean” and make sure it can handle locations that are not in any country.
 # >>>>>>>>>>>>>>>>>>>>>>>
+#PY4E code, geojson.py
 
-import urllib.request
+# NO CHANGES
+import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
-import time
 
-# ----
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
 
-# url = 'http://py4e-data.dr-chuck.net/comments_42.json'
-# url = 'http://py4e-data.dr-chuck.net/comments_1154833.json'
-url = input('enter URL below\n>>> ')
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-# ----
-
-# Ignore SSL certificate errors (from PY4E, because i do not understand, but need to use this code to test example)
-
+# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
+
 # ----
 
-response = urllib.request.urlopen(url, context=ctx)
-
-data = json.loads(response.read())
-# print(data)
-# print(type(data))
 
 # ----
 
-# print('GATHERING, {0}'.format(url))
-print('GATHERING')
-time.sleep(.5)
-print('......')
-time.sleep(1)
-print('...')
-time.sleep(1)
-
-# ----
-
-counts = 0
-sum = 0
-
-for i in data['comments'] :
-    # count += 1
-    # print(i, type(i))
-    c = int((i["count"]))
-    counts += 1
-    sum += c
-
-# ----
-
-print('number of users : {0}\nsum of all comments made: {1}'.format(counts, sum))
 
 # ...........................
