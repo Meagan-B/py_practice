997903de0ebdbc0156266e3367c26a54d102a534
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-05-20 13:09:50 -0700

    Update pythonpractice_018.py

--- pythonpractice_probs/pythonpractice_018.py
+++ pythonpractice_probs/pythonpractice_018.py
@@ -32,6 +32,7 @@ u_guess_lst = [int(d) for d in str(u_guess)]
 # ----
      # ----
 guess = 0
+
 while guess < 3 :
     guess += 1
     cow = 0
@@ -40,12 +41,22 @@ while guess < 3 :
     if u_guess == 'quit' : break
     if u_guess_lst == rand_num_gen :
         cow = 4
-
-    if cow == 4 :
-        print('!!!!!! YOU WIN !!!!!!')
-        break
     else :
-        print('you have {0} COW(s) and {1} BULL(s)'.format(cow, bull))
+        cow = 0
+        bull = 0
+
+
+        for i in range(0, 3) :
+            if u_guess_lst[i] == rand_num_gen[i]:
+                cow += 1
+            elif u_guess_lst[i] in rand_num_gen:
+                bull += 1
+
+
+if cow == 4 :
+    print('!!!!!! YOU WIN !!!!!!')
+else :
+    print('you have {0} COW(s) and {1} BULL(s)'.format(cow, bull))
 
     # for (i, n) in (u_guess_lst, rand_num_gen) :
     #     if i == n :
@@ -101,69 +112,3 @@ print('*end*')
 #         print("++++++++++++++++")
 #
 # game(4)
-#
-# guesses = 0
-#
-# while (u_guess != 'quit'):
-#     print('GUESS #{0}\n'.format(guesses))
-#     cowbullgame(u_guess, rand_lst)
-#
-# # RESULTS
-# # COWS
-#     if cow_bull[0] > 1 or cow_bull[0] == 0 :
-#         print('step 4a')
-#         print('you have {0} COWS'.format(cow_bull[0]))
-#     elif cow_bull[0] == 1 :
-#         print('step 4b')
-#         print('you have {0} COW'.format(cow_bull[0]))
-# # BULLS
-#     elif cow_bull[1] > 1 or cow_bull[1] == 0 :
-#         print('step 4c')
-#         print('\n& {0} BULLS'.format(cow_bull[1]))
-#     elif cow_bull[1] == 1 :
-#         print('step 4d')
-#         print('you have {0} BULL'.format(cow_bull[1]))
-# # WIN
-#     elif cow_bull[0] == 4 :
-#         print('!!!!!! YOU WIN !!!!!!')
-#
-#
-#     if cow_bull[0] == 4 :
-#         u_guess = 'quit'
-#
-#     u_guess = input('guess again\n>>> ')
-#     u_guess = list(u_guess.split())
-#
-#     guesses += 1
-# ----
-
-# cow_bull = [0,0]
-#
-# def cowbullgame(guess, random_lst) :
-#     # cow_bull = [0,0]
-#     global cow_bull
-#
-#     for i, n in zip(guess, random_lst) :
-#
-#         if i in random_lst :
-#
-#             if i == n :
-#                 cow_bull[0] += 1
-#             else :
-#                 cow_bull[1] += 1
-#
-#     # COWS
-#     if cow_bull[0] > 1 or cow_bull[0] == 0 :
-#         print('you have {0} COWS'.format(cow_bull[0]))
-#     elif cow_bull[0] == 1 :
-#         print('you have {0} COW'.format(cow_bull[0]))
-#     # BULLS
-#     elif cow_bull[1] > 1 or cow_bull[1] == 0 :
-#         print('\n& {0} BULLS'.format(cow_bull[1]))
-#     elif cow_bull[1] == 1 :
-#         print('you have {0} BULL'.format(cow_bull[1]))
-#     # WIN
-#     elif cow_bull[0] == 4 :
-#         print('!!!!!! YOU WIN !!!!!!')
-#
-# # ----
