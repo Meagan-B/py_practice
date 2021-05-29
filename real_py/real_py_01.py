50c33d3821c75ba67e79d6ec012c1839850487c5
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-05-28 21:25:06 -0700

    file created

---
+++ real_py/real_py_01.py
@@ -0,0 +1,12 @@
+# Sum of Integers Up To n
+#     Write a function, add_it_up, which returns the sum of the integers from 0
+#     to the single integer input parameter.
+#     The function should return 0 if a non-integer is passed in.
+
+# >>>>>>>>>>>>>>>>>>>>>>>
+
+
+# ----
+# ----
+# ----
+# ...........................
