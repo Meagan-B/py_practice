96738e9856a151df77f40ee4d2d7bc615ceed95b
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-03 16:10:14 -0700

    Update real_py_quiz.py

--- real_py/real_py_quiz.py
+++ real_py/real_py_quiz.py
@@ -1,9 +1,16 @@
-# x = 2**63 - 1
-# y = 2**90 - 1
-#
-# print(x, y)
+import re
 
-# >>>>>>>>>>>>>>>>>>>>>>>
+def letter_changes(my_string):
+    in_letters = "abcdefghijklmnopqrstuvxyz"
+    out_letters = "bcdefghijklmnopqrstuvxyza"
 
-x = 0xa5
-print(x)
+    letter_dict1 = {x:y for x,y in zip(in_letters, out_letters)}
+    letter_dict2 = {'a':'A', 'e':'E', 'i':'I', 'o':'O', 'u':'U'}
+
+    for l_dict in [letter_dict1, letter_dict2]:
+        pattern = re.compile("|".join(l_dict.keys()))
+        my_string = pattern.sub(lambda m: l_dict[re.escape(m.group(0))], my_string)
+
+    return my_string
+
+print(letter_changes('fork'))
