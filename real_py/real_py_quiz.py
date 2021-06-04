08b65253dafa0bbb62f3ef75ac5f1005e3261a26
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-04 14:58:30 -0700

    Update real_py_quiz.py

--- real_py/real_py_quiz.py
+++ real_py/real_py_quiz.py
@@ -1,13 +1,15 @@
 import re
 
 def letter_changes(my_string):
-    in_letters = "abcdefghijklmnopqrstuvxyz"
-    out_letters = "bcdefghijklmnopqrstuvxyza"
+    in_letters = "abc"
+    out_letters = "xyz"
 
     letter_dict1 = {x:y for x,y in zip(in_letters, out_letters)}
-    letter_dict2 = {'a':'A', 'e':'E', 'i':'I', 'o':'O', 'u':'U'}
+#    letter_dict2 = {'a':'A', 'e':'E', 'i':'I', 'o':'O', 'u':'U'}
 
-    for l_dict in [letter_dict1, letter_dict2]:
+    print(zip(in_letters, out_letters))
+
+    for l_dict in [letter_dict1]:
         pattern = re.compile("|".join(l_dict.keys()))
         my_string = pattern.sub(lambda m: l_dict[re.escape(m.group(0))], my_string)
 
