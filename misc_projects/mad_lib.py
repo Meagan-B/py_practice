85b670c4137a73c5695ec785bba397b39cb9839c
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-06 20:58:14 -0700

    Update mad_lib.py

--- misc_projects/mad_lib.py
+++ misc_projects/mad_lib.py
@@ -3,10 +3,16 @@
 # >>>>>>>>>>>>>>>>>>>>>>>
 
 import urllib.request
+
 # ----
+
 usr_host = str(input('enter URL below\n>>> '))
+print(usr_host)
 usr_host = usr_host.rstrip()
+print(usr_host)
+
 # ----
+
 with urllib.request.urlopen(usr_host) as response :
     html = response.read()
     html = html.decode()
