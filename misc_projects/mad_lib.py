0dcfc19306a97eb44014838add763793272da010
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-09 11:55:35 -0700

    Update mad_lib.py
    wokring on function to hold all word swapping

--- misc_projects/mad_lib.py
+++ misc_projects/mad_lib.py
@@ -21,22 +21,38 @@ with urllib.request.urlopen(usr_host) as response :
     
 # ----
 
-for_splice = html.split()
-#print(type(for_splice))
-#print(for_splice)
+def romeo_mad_lib()
+    global html
+    
+    for_splice = html.split()
 
+    adj = [1, 5, 18, 23, 28, 30]
+    adj_mapping = map(for_splice.__getitem__, adj)
+    adj_map_list = list(adj_mapping) 
+    print('adjectives: {0}'.format(adj_map_list))
+    
+    adj_dict = {x:y for x,y in zip(in_alpha, out_alpha)}
+    #print(letter_dict1)
+    
+    #CODE FROM O_STACK >>>
+    for a_tup in [adj_dict]:
+        pattern = re.compile("|".join(w_dict.keys()))
+        #print(pattern)
+        my_string = pattern.sub(lambda m: w_dict[re.escape(m.group(0))], t)
+        #print(my_string)
+# ----
+    noun = [3, 6, 16, 19, 24, -1]
+    noun_mapping = map(for_splice.__getitem__, noun)
+    noun_map_list = list(noun_mapping)
+    print('nouns: {0}'.format(noun_map_list))
+# ----
+    verb = [7, 17, 21]
+    verb_mapping = map(for_splice.__getitem__, verb)
+    verb_map_list = list(verb_mapping)
+    print('verbs: {0}'.format(verb_map_list))
 
-adj = [1, 5, 18, 23, 28, 30]
-adj_mapping = map(for_splice.__getitem__, adj)
-adj_map_list = list(adj_mapping)
-print('adjectives: {0}'.format(adj_map_list))
-
-noun = [3, 6, 16, 19, 24, -1]
-noun_mapping = map(for_splice.__getitem__, noun)
-noun_map_list = list(noun_mapping)
-print('nouns: {0}'.format(noun_map_list))
-
-verb = [7, 17, 21]
-verb_mapping = map(for_splice.__getitem__, verb)
-verb_map_list = list(verb_mapping)
-print('verbs: {0}'.format(verb_map_list))
+# ----
+    
+    
+    
+    return my_string
