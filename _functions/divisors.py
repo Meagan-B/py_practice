fca15ce9b44d06efe0e57348176a312ca89cd830
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-09 16:35:16 -0700

    Update divisors.py

--- _functions/divisors.py
+++ _functions/divisors.py
@@ -1,7 +1,7 @@
 # Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
 # For example, 13 is a divisor of 26 because 26 / 13 has no remainder.
 #---->>>>>>>>>>>>-------->>>>>>>>>>>>----#
-welcome = 'welcome to the,\n••••§•••• DIVISOR CHECKER ••••§••••\n\n'
+welcome = '••••§•••• DIVISOR CHECKER ••••§••••\n'
 print(welcome)
 #----••••••••----••••••••----••••••••----#
 def divisor_check(n) :
