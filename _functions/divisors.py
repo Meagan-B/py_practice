469a2d0cd3b374323228180573d7f9d0d878dd3e
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-10 11:59:23 -0700

    Update divisors.py

--- _functions/divisors.py
+++ _functions/divisors.py
@@ -5,29 +5,29 @@ welcome = '••••§•••• DIVISOR CHECKER ••••§••••
 print(welcome)
 #----••••••••----••••••••----••••••••----#
 def divisor_check(n) :
+    #print('n >>> {0}'.format(n))
     
     n_form = [ int(d) for d in n]
-    print('n_form >>> {0}'.format(n))    
+    #print('n_form >>> {0}'.format(n_form))    
     
-    divisors = list(range(1, n))
-    print('divisors >>> {0}'.format(divisors))
-    b = []
+    for e in n_form :
+        divisors = list(range(1, e))
+        #print('divisors >>> {0}'.format(divisors))
+    
+        b = []
 
-
-    for i in divisors :
-        if n % i == 0 :
-            b.append(i)
-        else :
-            continue
-
-    print(b)
+        for i in divisors :
+            if e % i == 0 :
+                b.append(i)
+            else :
+                continue
+            
+        print('divisors for {0} >>> {1}'.format(e, b))
 #----••••••••----••••••••----••••••••----#
 div_inp = input('enter the number(s) you would like to check below,\n>>> ')
+#print('div_inp >>> {0}'.format(div_inp)) 
 div_lst = div_inp.split()
-print('div_lst >>> {0}'.format(div_lst)) 
-div_int_lst = [int(i) for i in div_lst]
-print('div_int_lst >>> {0}'.format(div_int_lst)) 
-#print(div_inp, div_lst, div_int_lst)
+#print('div_lst >>> {0}'.format(div_lst)) 
 #----••••••••----••••••••----••••••••----#
-divisor_check(div_int_lst)
+divisor_check(div_lst)
 #---->>>>>>>>>>>>-------->>>>>>>>>>>>----#
