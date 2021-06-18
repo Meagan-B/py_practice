15dad770991d5d1c1049fc5cedd345f131ef6db2
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-18 15:47:22 -0700

    Update time_to_100.py

--- _functions/time_to_100.py
+++ _functions/time_to_100.py
@@ -1,9 +1,3 @@
-#Create a program that asks the user to enter their name and their age. Print out a
-#message addressed to them that tells them the year that they will turn 100 years old.
-#Add on to the previous program by asking the user for another number and printing out that
-#many copies of the previous message. (Hint: order of operations exists in Python)
-#Print out that many copies of the previous message on separate lines. (Hint: the string
-#"\n is the same as pressing the ENTER button)
 #---->>>>>>>>>>>>-------->>>>>>>>>>>>----#
 def time_to_100(p):
     from datetime import date
