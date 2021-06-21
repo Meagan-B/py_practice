7a5b218f6b30bf20b7a843480fe61b35e8518554
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-20 22:32:23 -0700

    Update hourly_rate_f.py

--- _functions/hourly_rate_f.py
+++ _functions/hourly_rate_f.py
@@ -1,15 +1,9 @@
-#Write a program to prompt the user for hours and rate per hour using input
-======
-#2.3 Write a program to prompt the user
-#for hours and rate per hour using input
-======
-# begining of try loop, where C will attempt the prompts below try and above accept
-#
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# hrs variable will contain an integer given by user via the input() funtion, here it is total hours worked. rate variable will contain a float given by user via the input() funtion, here an hourly rate. if the try fails, except will take over, maybe user enters a character instead of a number, this is a broad except.
-try :
-    hrs = int(input('hours: '))
-    rate = float(input('hourly rate: '))
+#---->>>>>>>>>>>>-------->>>>>>>>>>>>----#
+print('~~~~~ HOURLY RATE CALCULATOR ~~~~~\n••••••••••••••••••••••••••••••')
+#----••••••••----••••••••----••••••••----# 
+def hourly_rate() :
+    hrs = float(input('hours\n>>> '))
+    rate = float(input('hourly rate\n>>> '))
 except :
     print('numbers only')
 
