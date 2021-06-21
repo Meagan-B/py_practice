7e0f411576cbe3e2bcad67851952f15918688baf
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-20 23:04:01 -0700

    Update hourly_rate_f.py

--- _functions/hourly_rate_f.py
+++ _functions/hourly_rate_f.py
@@ -3,16 +3,17 @@ welcome = '~~~~~ HOURLY RATE CALCULATOR ~~~~~\n•••••••••••
 print(welcome)
 #----••••••••----••••••••----••••••••----#
 inp_hrs = input('hours\n>>> ')
-    inp_rate = input('hourly rate\n>>> ')
+inp_rate = input('hourly rate\n>>> ')
+inp_currency = input('currency of pay (default is USD)\n>>> ')
 #----••••••••----••••••••----••••••••----# 
-def hourly_rate(h, r) :
+def hourly_rate(h, r, c) :
     
 #----••••••••----#      
     if hrs == and rate == :
         hrs = float(inp_hrs)
         rate = float(inp_rate)
 #----••••••••----#      
-    print('HRS >>> {0}\nRATE >>> {1}\nHOURLY RATE >>> {2} '.format(hrs, rate, (hrs * rate)))
+    print('HRS >>> {0}\nnHOURLY RATE >>> {1}\nTOTAL PAY >>> {2} '.format(hrs, rate, (hrs * rate)))
 #----••••••••----#      
     else:
         print('ERROR\n>>> accepts numbers only'.format())
@@ -24,6 +25,11 @@ from forex_python.converter import CurrencyRates
 >>> c = CurrencyRates()
 >>> c.get_rates('USD')   # you can directly call get_rates('USD')
 
-hourly_rate()
+>>> from forex_python.bitcoin import BtcConverter
+>>> b = BtcConverter()   # add "force_decimal=True" parmeter to get Decimal rates
+>>> b.get_latest_price('EUR')   # you can directly call get_latest_price('EUR')
+476.5225  # return type float
 
+#----••••••••----••••••••----••••••••----# 
+hourly_rate(inp_hrs, inp_rate, inp_currency)
 #---->>>>>>>>>>>>-------->>>>>>>>>>>>----#
