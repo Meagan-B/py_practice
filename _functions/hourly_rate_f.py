477c3a2a746892f1e64bba960ad267b1d22a3bdb
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-20 22:54:23 -0700

    Update hourly_rate_f.py

--- _functions/hourly_rate_f.py
+++ _functions/hourly_rate_f.py
@@ -2,15 +2,22 @@
 print('~~~~~ HOURLY RATE CALCULATOR ~~~~~\n••••••••••••••••••••••••••••••')
 #----••••••••----••••••••----••••••••----# 
 def hourly_rate() :
-    hrs = float(input('hours\n>>> '))
-    rate = float(input('hourly rate\n>>> '))
+    inp_hrs = input('hours\n>>> ')
+    inp_rate = input('hourly rate\n>>> ')
+    
+    if hrs == and rate == :
+        hrs = float(inp_hrs)
+        rate = float(inp_rate)
     
     = hrs * rate
     
-    print('numbers only')
+    else:
+        print('ERROR\n>>> accepts numbers only')
+        
+    print('pay: ', hrs * rate)
+#----••••••••----••••••••----••••••••----# 
 
-#----
-
-print('pay: ', hrs * rate)
-
+from forex_python.converter import CurrencyRates
+>>> c = CurrencyRates()
+>>> c.get_rates('USD')   # you can directly call get_rates('USD')
 #.........................
