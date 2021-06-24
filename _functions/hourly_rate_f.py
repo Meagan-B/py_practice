96dbecdf7b06d1c34ce055e7afe241a8968fc137
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-23 20:46:58 -0700

    Update hourly_rate_f.py

--- _functions/hourly_rate_f.py
+++ _functions/hourly_rate_f.py
@@ -4,6 +4,7 @@ print(welcome)
 #----••••••••----••••••••----••••••••----#
 inp_hrs = input('hours for the week of {0}\n>>> '.format())
 inp_rate = input('hourly rate\n>>> ')
+#----••••••••----••••••••----••••••••----# 
 y_or_n_overtime = input('do you recieve bonus pay for overtime?\n(Y/N) >>> ')
 if y_or_n_overtime == 'Y' or 'y' :
     inp_overtime = input('overtime multiplier\n>>> ')
@@ -11,17 +12,15 @@ else :
     inp_overtime = None 
 #----••••••••----••••••••----••••••••----# 
 def hourly_rate(h, r, o) :
-    
-#----••••••••----#
     if h.isdigit() == True and r.isdigit() == True and inp_overtime == True :
         hrs = float(inp_hrs)
         rate = float(inp_rate)
         ot_multi = float(inp_overtime)
 #----••••        
         if hrs > 40 :
-            ot_rate = 
-    
-        print('HRS >>> {0}\nHOURLY RATE >>> {1}\nTOTAL PAY >>> {2} '.format(hrs, rate, (hrs * rate)))
+            ot_pay = (hrs - 40) * ot_multi 
+            print('HRS >>> {0}\nHOURLY RATE >>> {1}\nTOTAL PAY >>> {2} '.format(hrs, rate, (hrs * rate)))
+            print('HRS >>> {0}\nHOURLY RATE >>> {1}\nTOTAL PAY >>> {2} '.format(hrs, rate, ((hrs * rate) + ot_pay)))
 #----••••••••----#      
     else :
         print('ERROR\n>>> accepts numbers only'.format())
