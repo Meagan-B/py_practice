1ee4d43c44c6ae53aad17c977846ad564f1c69cb
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-23 20:36:48 -0700

    Update hourly_rate_f.py

--- _functions/hourly_rate_f.py
+++ _functions/hourly_rate_f.py
@@ -6,14 +6,15 @@ inp_hrs = input('hours for the week of {0}\n>>> '.format())
 inp_rate = input('hourly rate\n>>> ')
 y_or_n_overtime = input('do you recieve bonus pay for overtime?\n(Y/N) >>> ')
 if y_or_n_overtime == 'Y' or 'y' :
-    inp_overtime = input('overtime?\n(Y/N) >>> ')
+    inp_overtime = input('overtime multiplier\n>>> ')
 #----••••••••----••••••••----••••••••----# 
 def hourly_rate(h, r, o) :
     
 #----••••••••----#
-    if h.isdigit() == True and r.isdigit() == True :
+    if h.isdigit() == True and r.isdigit() == True and inp_overtime == True :
         hrs = float(inp_hrs)
         rate = float(inp_rate)
+        ot_multi = float(inp_overtime)
 #----••••        
         if hrs > 40 :
             ot_rate = 
