bc4b21322c7123c5deaa75c690371d3a484bc9c3
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-23 20:32:33 -0700

    Update hourly_rate_f.py

--- _functions/hourly_rate_f.py
+++ _functions/hourly_rate_f.py
@@ -2,20 +2,26 @@
 welcome = '~~~~~ HOURLY RATE CALCULATOR ~~~~~\n••••••••••••••••••••••••••••••'
 print(welcome)
 #----••••••••----••••••••----••••••••----#
-inp_hrs = input('hours\n>>> ')
+inp_hrs = input('hours for the week of {0}\n>>> '.format())
 inp_rate = input('hourly rate\n>>> ')
+y_or_n_overtime = input('do you recieve bonus pay for overtime?\n(Y/N) >>> ')
+if y_or_n_overtime == 'Y' or 'y' :
+    inp_overtime = input('overtime?\n(Y/N) >>> ')
 #----••••••••----••••••••----••••••••----# 
-def hourly_rate(h, r) :
+def hourly_rate(h, r, o) :
     
 #----••••••••----#
     if h.isdigit() == True and r.isdigit() == True :
         hrs = float(inp_hrs)
         rate = float(inp_rate)
+#----••••        
+        if hrs > 40 :
+            ot_rate = 
     
         print('HRS >>> {0}\nHOURLY RATE >>> {1}\nTOTAL PAY >>> {2} '.format(hrs, rate, (hrs * rate)))
 #----••••••••----#      
     else :
         print('ERROR\n>>> accepts numbers only'.format())
 #----••••••••----••••••••----••••••••----# 
-hourly_rate(inp_hrs, inp_rate)
+hourly_rate(inp_hrs, inp_rate, inp_overtime)
 #---->>>>>>>>>>>>-------->>>>>>>>>>>>----#
