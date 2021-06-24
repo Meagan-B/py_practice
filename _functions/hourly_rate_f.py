7ec73d1b74ffa6b724fad23d3018e4a5c0a6953f
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-23 20:54:58 -0700

    Update hourly_rate_f.py

--- _functions/hourly_rate_f.py
+++ _functions/hourly_rate_f.py
@@ -2,22 +2,24 @@
 welcome = '~~~~~ HOURLY RATE CALCULATOR ~~~~~\n••••••••••••••••••••••••••••••'
 print(welcome)
 #----••••••••----••••••••----••••••••----#
-inp_hrs = input('hours for the week of {0}\n>>> '.format())
+from datetime import date
+inp_hrs = input('hours for the week of {0}\n>>> '.format(date.today()))
+#----••••••••----••••••••----••••••••----# 
 inp_rate = input('hourly rate\n>>> ')
 #----••••••••----••••••••----••••••••----# 
 y_or_n_overtime = input('do you recieve bonus pay for overtime?\n(Y/N) >>> ')
-if y_or_n_overtime == 'Y' or 'y' :
+if y_or_n_overtime[0] == 'Y' or y_or_n_overtime[0] == 'y' :
     inp_overtime = input('overtime multiplier\n>>> ')
 else :
     inp_overtime = None 
 #----••••••••----••••••••----••••••••----# 
 def hourly_rate(h, r, o) :
-    if h.isdigit() == True and r.isdigit() == True and inp_overtime == True :
+    if h.isdigit() == True and r.isdigit() == True:
         hrs = float(inp_hrs)
         rate = float(inp_rate)
-        ot_multi = float(inp_overtime)
 #----••••        
-        if hrs > 40 :
+        if hrs > 40 and inp_overtime is not None:
+            ot_multi = float(inp_overtime)
             ot_pay = (hrs - 40) * ot_multi 
             print('HRS >>> {0}\nHOURLY RATE >>> {1}\nTOTAL PAY >>> {2} '.format(hrs, rate, (hrs * rate)))
             print('HRS >>> {0}\nHOURLY RATE >>> {1}\nTOTAL PAY >>> {2} '.format(hrs, rate, ((hrs * rate) + ot_pay)))
