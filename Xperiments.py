55edb4bb68abedcdae4abeb8a9b0bab61b5bbe4f
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-30 20:23:15 -0700

    Update Xperiments.py

--- Xperiments.py
+++ Xperiments.py
@@ -1,16 +1,20 @@
 import numpy
 
 board = [i for i in range(1,10)]
+print(board)
+
 move = input('make your move, input digit from 1-9\n>>> ')
  
 for i in board :
     if move == i : 
         arr = numpy.asarray(board)
-        print(arr)
+        print(numpy.asarray(board))
         arr[ arr == 'X' ] = move
         print(arr)
 
 print(move)
 print(board)
 
-                
+#arr = numpy.asarray([1, 6, 1, 9, 8])
+#arr[ arr == 8 ] = 0 
+#print(arr)
