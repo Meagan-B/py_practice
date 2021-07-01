f80013543931773cff70e66f0d3ccee7187db5a2
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-06-30 20:20:02 -0700

    Update Xperiments.py

--- Xperiments.py
+++ Xperiments.py
@@ -1,12 +1,16 @@
 import numpy
 
 board = [i for i in range(1,10)]
-
 move = input('make your move, input digit from 1-9\n>>> ')
  
-arr = numpy.asarray(board)
-print(arr)
-arr[ arr == 'b' ] = move # change all occurrences of x by y
-print(arr)
-ply_count += 1
+for i in board :
+    if move == i : 
+        arr = numpy.asarray(board)
+        print(arr)
+        arr[ arr == 'X' ] = move
+        print(arr)
+
+print(move)
+print(board)
+
                 
