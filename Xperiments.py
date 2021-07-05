16f8b8209067af4a1a753f675e37d9e447e33ba9
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-07-05 09:30:10 -0700

    Update Xperiments.py

--- Xperiments.py
+++ Xperiments.py
@@ -1,16 +1,23 @@
 #import numpy
 
+def board_print(b):
+    #b = [[i] for i in b]
+    print(b[:3])
+    print(b[3:6])
+    print(b[6:])
+    
 board = [i for i in range(1,10)]
-print(board)
+board_print(board)
 
 move = input('make your move, input digit from 1-9\n>>> ')
 move = int(move) 
 
-
-def f1(arr, find, replace):
-    print('ORIGINAL array: {0}\nfind: {1}\nreplacement: {2}'.format(arr, find, replace))
+def char_replace(arr, find, replace):
+    #print('ORIGINAL array: {0}\nfind: {1}\nreplacement: {2}'.format(arr, find, replace))
     for cnt in range(arr.count(find)):
         arr[find - 1] = replace
-        print('MODIFIED array: {0}'.format(arr))
-
-f1(board, move, 'X')
+        #print('MODIFIED array: {0}'.format(arr))
+        board_print(arr)
+        
+        
+char_replace(board, move, 'x')
