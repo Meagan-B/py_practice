b21dad5830f74ab391e58e8ae175c29595aedbad
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-07-05 09:08:11 -0700

    Update Xperiments.py

--- Xperiments.py
+++ Xperiments.py
@@ -6,26 +6,11 @@ print(board)
 move = input('make your move, input digit from 1-9\n>>> ')
 move = int(move) 
 
-#for i in board :
-   # if move == i :
-  #      print(i)
- #       i = 'X'
-#        print(i)
-        #need to find way to replace the i in board, reverse the for i loop to put it back with new value
-
-#print(move)
-#print(board)
-
 
 def f1(arr, find, replace):
-    print('ORIGINAL array: {0}\nfind: {1}\nreplace: {2}'.format(arr, find, replace))
+    print('ORIGINAL array: {0}\nfind: {1}\nreplacement: {2}'.format(arr, find, replace))
     for cnt in range(arr.count(find)):
         arr[find - 1] = replace
         print('MODIFIED array: {0}'.format(arr))
 
 f1(board, move, 'X')
-
-
-#arr = numpy.asarray([1, 6, 1, 9, 8])
-#arr[ arr == 8 ] = 0 
-#print(arr)
