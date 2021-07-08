840bc87fd1d05f382bd595357128ca8289bda85a
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-07-08 09:51:55 -0700

    Update tic_tac_toe_f.py

--- _functions/tic_tac_toe_f.py
+++ _functions/tic_tac_toe_f.py
@@ -44,17 +44,21 @@ def char_replace(arr, find, replace):
 # win check
 
 ######START HERE######
-def win_chk() :
+def win_chk(p) :
+    
+    wins = (1,2,3),(1,5,9),(1,4,7),(2,5,8),(3,6,9),(3,5,7),(4,5,6),(7,8,9)
     
 #----••••••••----••••••••----••••••••----#
 # game play
 
 def play(h, c) :
     ply_count = 0
+    ply_track = []
+    
     board = [i for i in range(1,10)]
     board_print(board)
     
-    while ply_count < 1 :
+    while ply_count < 3 :
         move = input('make your move, input digit from 1-9\n>>> ')
         
         x = move.isnumeric()
