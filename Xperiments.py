142c74c04256867442aae6df6a740015a2b7fe84
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-07-13 14:42:12 -0700

    Update Xperiments.py

--- Xperiments.py
+++ Xperiments.py
@@ -1,8 +1,6 @@
 def win_chk(p) :
     wins = [(1,2,3),(1,5,9),(1,4,7),(2,5,8),(3,6,9),(3,5,7),(4,5,6),(7,8,9)]
-    print(wins)
     wins = set(wins)
-    print(wins)
     p = [int(m) for m in p]
     print(p)
     
