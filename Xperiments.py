4b93ed59d1d5315a92a813806d5ac8b3891e37fb
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-07-13 14:57:00 -0700

    Update Xperiments.py

--- Xperiments.py
+++ Xperiments.py
@@ -1,8 +1,14 @@
 def win_chk(p) :
     wins = [(1,2,3),(1,5,9),(1,4,7),(2,5,8),(3,6,9),(3,5,7),(4,5,6),(7,8,9)]
-    wins = set(wins)
+    #wins = set(wins)
     p = [int(m) for m in p]
     print(p)
+    
+    for l in wins :
+        l = set(l)
+        print(l)
+        p = set(p)
+        print(p)
     
 
 ply_count = 0
@@ -11,7 +17,6 @@ ply_track = []
 while ply_count < 3 :
     move = input('make your move, input digit from 1-9\n>>> ')
     ply_track += move
-    print(ply_track)
     ply_count += 1
 
 
