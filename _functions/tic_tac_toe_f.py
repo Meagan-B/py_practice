c8553c91fc4219a6693579038a2e7278994848d5
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-07-14 17:00:21 -0700

    Update tic_tac_toe_f.py

--- _functions/tic_tac_toe_f.py
+++ _functions/tic_tac_toe_f.py
@@ -43,10 +43,16 @@ def char_replace(arr, find, replace):
 #----••••••••----••••••••----••••••••----#
 # win check
 
-######START HERE######
 def win_chk(p) :
+    wins = [(1,2,3),(1,5,9),(1,4,7),(2,5,8),(3,6,9),(3,5,7),(4,5,6),(7,8,9)]
+    p = [int(m) for m in p]
+    print(p)
     
-    wins = (1,2,3),(1,5,9),(1,4,7),(2,5,8),(3,6,9),(3,5,7),(4,5,6),(7,8,9)
+    for l in wins :
+        l = set(l)
+        p = set(p)
+        if l == p :
+            print('win')
     
 #----••••••••----••••••••----••••••••----#
 # game play
@@ -81,7 +87,6 @@ def play(h, c) :
             continue        
     
 play(human, comp)    
-
 
 #---->>>>>>>>>>>>-------->>>>>>>>>>>>----#
 
