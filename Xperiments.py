5b599fec447a1214de334bf08782668b0694178a
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-07-21 13:24:51 -0700

    Update Xperiments.py

--- Xperiments.py
+++ Xperiments.py
@@ -1,13 +1,13 @@
-def win_chk(p) :
-    wins = [(1,2,3),(1,5,9),(1,4,7),(2,5,8),(3,6,9),(3,5,7),(4,5,6),(7,8,9)]
-    p = [int(m) for m in p]
-    print(p)
+#def win_chk(p) :
+ #   wins = [(1,2,3),(1,5,9),(1,4,7),(2,5,8),(3,6,9),(3,5,7),(4,5,6),(7,8,9)]
+  #  p = [int(m) for m in p]
+   # print(p)
     
-    for l in wins :
-        l = set(l)
-        p = set(p)
-        if l == p :
-            print('win')
+    #for l in wins :
+     #   l = set(l)
+      #  p = set(p)
+       # if l == p :
+        #    print('win')
             
 #++++++++++++++++++++++++++++++
 #ply_count = 0
