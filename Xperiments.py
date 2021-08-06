1c3264c6412477d260edc5afe759fb396b4b7cb4
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-08-05 20:11:23 -0700

    Update Xperiments.py

--- Xperiments.py
+++ Xperiments.py
@@ -31,3 +31,31 @@ print(win_chk(p1))
 print(win_chk(p2))
 print(win_chk(p3))
 print(win_chk(p4))
+
+#+++++++++++++++++++++++++++
+wins = [(1,2,3),(1,5,9),(1,4,7),(2,5,8),(3,6,9),(3,5,7),(4,5,6),(7,8,9)]
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#ORIGINAL CODE
+
+def can_win(brd, player):
+    places=[]
+    x=0
+    for i in brd:
+        if i == player: places.append(x);
+        x+=1
+    win=True
+    for tup in wins:
+        win=True
+        for ix in tup:
+            if brd[ix] != player:
+                win=False
+                break
+        if win == True:
+            break
+    return win
+
+print(can_win(board, p1))
+print(can_win(board, p2))
+print(can_win(board, p3))
+print(can_win(board, p4))
