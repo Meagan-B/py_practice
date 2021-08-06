2766a20bdb0e747b43261f3f528d070d32d7b94c
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-08-06 15:11:06 -0700

    new files

--- py4e/py4e_14/py4e_14_01.py
+++ py4e/py4e_14/py4e_14_01.py
@@ -38,3 +38,22 @@ print(stuff)
 print (stuff[1])
 print (stuff.__getitem__(0))
 print (list.__getitem__(stuff,0))
+
+#4
+class PartyAnimal:
+   x = 0
+   name = ''
+   def __init__(self, nam):
+     self.name = nam
+     print(self.name,'constructed')
+
+   def party(self) :
+     self.x = self.x + 1
+     print(self.name,'party count',self.x)
+
+s = PartyAnimal('Sally')
+j = PartyAnimal('Jim')
+
+s.party()
+j.party()
+s.party()
