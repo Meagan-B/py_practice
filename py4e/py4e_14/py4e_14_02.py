1bc665f9b0c4ed45c7d254f55cb0516ddf28e084
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-08-06 15:35:44 -0700

    Update py4e_14_02.py

--- py4e/py4e_14/py4e_14_02.py
+++ py4e/py4e_14/py4e_14_02.py
@@ -1,4 +1,15 @@
-#2
+class PartyAnimal:
+   x = 0
+   name = ''
+   def __init__(self, nam):
+     self.name = nam
+     print(self.name,'constructed')
+
+   def party(self) :
+     self.x = self.x + 1
+     print(self.name,'party count',self.x)
+
+
 class CricketFan(PartyAnimal):
    points = 0
    def six(self):
