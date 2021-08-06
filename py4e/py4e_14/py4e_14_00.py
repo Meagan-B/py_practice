f0cd231c6fcd65de41151d6e4b0b49f34db2640f
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2021-08-06 15:07:01 -0700

    Update py4e_14_00.py

--- py4e/py4e_14/py4e_14_00.py
+++ py4e/py4e_14/py4e_14_00.py
@@ -1,3 +1,4 @@
+#1
 class PartyAnimal:
    x = 0
 
@@ -10,3 +11,30 @@ an.party()
 an.party()
 an.party()
 PartyAnimal.party(an)
+
+from party import PartyAnimal
+
+#2
+class CricketFan(PartyAnimal):
+   points = 0
+   def six(self):
+      self.points = self.points + 6
+      self.party()
+      print(self.name,"points",self.points)
+
+s = PartyAnimal("Sally")
+s.party()
+j = CricketFan("Jim")
+j.party()
+j.six()
+print(dir(j))
+
+#3
+stuff = list()
+stuff.append('python')
+stuff.append('chuck')
+stuff.sort()
+print(stuff)
+print (stuff[1])
+print (stuff.__getitem__(0))
+print (list.__getitem__(stuff,0))
