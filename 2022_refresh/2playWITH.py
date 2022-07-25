dc23250ff6b254a18efd7678c66bfea585c2cdef
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2022-07-25 16:29:32 -0700

    Update 2playWITH.py

--- 2022_refresh/2playWITH.py
+++ 2022_refresh/2playWITH.py
@@ -4,11 +4,13 @@ rate = float(input("Enter Pay/Rate:"))
 if hrs <= 40 :
     wages = hrs * rate
 
-
 else :
-    print(rate)
-    rate = rate * 1.5
-    print(rate)
-    wages = hrs * rate
+    reg_wages = 40 * rate
+    
+    ot_hrs = hrs - 40
+    ot_rate = rate * 1.5
+    ot_wages = ot_hrs * ot_rate
+    
+    wages = ot_wages + reg_wages
 
 print(float(wages))
