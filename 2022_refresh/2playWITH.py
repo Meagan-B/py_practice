0a04c750cf13c57b5bc9f42b014513f1c06219c9
Meagan-B <78054112+Meagan-B@users.noreply.github.com>
2022-07-25 15:36:26 -0700

    stating up again

---
+++ 2022_refresh/2playWITH.py
@@ -0,0 +1,7 @@
+# This first line is provided for you
+
+hrs = float(input("enter hours:"))
+rate = float(input("enter hourly rate:"))
+
+payrate = float(hrs * rate)
+print(payrate)
--- py4e/py4e_01/py4e_1.py
+++ py4e/py4e_01/py4e_1.py
@@ -1,3 +1,5 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>
 # prints 'hello world'
 print('hello world')
+
+print(' i will remember all this shit i forgot')
